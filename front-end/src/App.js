caf39b58c2dd06253c5cf5ac70c08db2b433ad6a
phellipe5k <luizinhophe@hotmail.com>
2021-03-11 16:58:42 -0300

    Iniciando projeto

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
           Edit
-          <code>src/App.js</code>
+          <code>Hello World</code>
           and save to reload.
         </p>
         <a
