f245c68d3016afb7914c55bbf7af2ecfa8d7c411
danieleperse <danieleperse3@gmail.com>
2021-03-11 19:11:55 -0300

    estrutura inicial ok

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import history from '../services/history';
 
 export default function Login() {
   const [isEmailValid, setIsEmailValid] = useState(false);
@@ -7,8 +7,8 @@ export default function Login() {
 
   const handleChangeEmail = (event) => {
     const { value } = event.target;
-    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
-    if (regex.test(value.toLowerCase())) {
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (regex.test(value)) {
       setIsEmailValid(true);
     } else {
       setIsEmailValid(false);
@@ -26,10 +26,18 @@ export default function Login() {
     }
   };
 
+  const handleClick = () => {
+    history.push('/products');
+  };
+
+  const handleNoCount = () => {
+    history.push('/register');
+  };
+
   return (
     <div className="login">
       <label htmlFor="email">
-        E-mail
+        Email
         <input
           type="email"
           id="email"
@@ -46,24 +54,21 @@ export default function Login() {
           onChange={ handleChangePassword }
         />
       </label>
-      <Link>
-        <button
-          type="button"
-          data-testid="signin-btn"
-          disabled={ !(isEmailValid && isPasswordValid) }
-          onClick={}
-        >
-          Entrar
-        </button>
-      </Link>
-      <Link>
-        <button
-          type="button"
-          data-testid="no-account-btn"
-        >
-          Ainda não tenho conta
-        </button>
-      </Link>
+      <button
+        type="button"
+        data-testid="signin-btn"
+        disabled={ !(isEmailValid && isPasswordValid) }
+        onClick={ handleClick }
+      >
+        ENTRAR
+      </button>
+      <button
+        type="button"
+        data-testid="no-account-btn"
+        onClick={ handleNoCount }
+      >
+        Ainda não tenho conta
+      </button>
     </div>
-  )
-};
+  );
+}
