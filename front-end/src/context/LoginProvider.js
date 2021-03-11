9fadad9340043d43505855034210fcfa958a2710
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-03-11 17:21:18 -0300

    setup inicial

---
+++ front-end/src/context/LoginProvider.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import TrybeerContext from './TrybeerContext.js';
+
+const LoginProvider = ({ children }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const providerValue = {
+    email, setEmail, password, setPassword,
+  };
+  return (
+    <TrybeerContext.Provider value={ providerValue }>
+      { children }
+    </TrybeerContext.Provider>
+  );
+};
+
+LoginProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default LoginProvider;
---
+++ front-end/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React, { useState, useContext } from 'react';
+import TrybeerContext from '../context/TrybeerContext';
+
+function Login() {
+  return (
+    <div>
+      Página de Login
+    </div>
+  );
+}
+
+export default Login;
