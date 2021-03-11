349cc65fe1acc5973de7b947889b75b9907db9c5
labelle <beelsdelonge@gmail.com>
2021-03-11 19:17:48 -0300

    Arrumando validacao login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/login" component={Login} />
+        <Route path="/login" component={ Login } />
+        <Route path="/register" component={ Register } />
       </Switch>
     </Router>
   );
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,33 +1,31 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
-function Login() {
-  const [user, setUser] = useState({ email: '', password: ''});
+function Login({ history }) {
+  const [user, setUser] = useState({ email: '', password: '' });
   const [valid, setValid] = useState(true);
 
+  useEffect(() => {
+    isValid();
+  }, [user.password, user.email]);
+
   const handleChange = ({ target }) => {
-    setUser({...user, [target.name]: target.value});
-    console.log(user.password);
-    if (user.password && user.password.lenght > 6) {
-      console.log('ola');
-      isValid();
-    }
+    setUser({ ...user, [target.name]: target.value });
   };
 
   const isValid = () => {
     const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-    const email = emailRegex(user.email);
+    const email = emailRegex.test(user.email);
     const senha = user.password;
-    const six = 6
-    if(email && senha.length > 5) {
-      setValid(false);
-      console.log('oi')
+    if(senha.length >= 6 && email) {
+      return setValid(false);
     } else {
-      setValid(true);
+      return setValid(true);
     }
   }
 
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    history.push('/register')
     localStorage.setItem('user', JSON.stringify(user.email));
   };
 
@@ -37,34 +35,32 @@ function Login() {
         <input
           type="email"
           name="email"
-          value={user.email}
+          value={ user.email }
           data-testid="email-input"
-          onChange={handleChange}
+          onChange={ handleChange }
         />
       </label>
       <label>Senha:
         <input
           type="password"
           name="password"
-          value={user.password}
+          value={ user.password }
           data-testid="password-input"
-          onChange={handleChange}
+          onChange={ handleChange }
           />
       </label>
-      <button
-        type="submit"
-        data-testid="signin-btn"
-        disabled={valid}
-        onClick={handleClick}
-      >Entar
+      <button type="submit" data-testid="signin-btn" disabled={ valid } onClick={ handleClick }>
+        Entar
       </button>
       <button
         type="button"
         data-testid="no-account-btn"
-      >Ainda nao tenho conta
+        onClick={ handleClick }
+      >
+        Ainda nao tenho conta
       </button>
     </div>
-  )
+  );
 }
 
 export default Login;
