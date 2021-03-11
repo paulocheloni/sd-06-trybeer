dbf69e1a1a4cf3a5175f59606754a98762cd76a7
Tiago Berwanger <berwangertiago@gmail.com>
2021-03-11 18:10:54 -0300

    structuring backend application using MSC

---
+++ back-end/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const LoginController = require('./src/controllers/LoginController')
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use('/login', LoginController);
+
+app.listen(port, () => console.log(`Running at ${port}`))
---
+++ back-end/src/models/Users.js
@@ -0,0 +1,13 @@
+const connection = require('./connection');
+
+
+const findUser = async (email) => {
+  const [user] = await connection 
+    .execute('SELECT * FROM users WHERE email=?', 
+    [email] )
+  return user;
+};
+
+module.exports ={
+  findUser
+};
