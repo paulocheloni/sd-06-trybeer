3b2ca4846d75dae67776e78bfad279a409c87978
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-03-11 19:18:28 -0300

    inicio da tela de login

--- front-end/src/context/LoginProvider.js
+++ front-end/src/context/LoginProvider.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import TrybeerContext from './TrybeerContext.js';
 
 const LoginProvider = ({ children }) => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState(true);
   const [password, setPassword] = useState('');
   const providerValue = {
     email, setEmail, password, setPassword,
--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -2,9 +2,71 @@ import React, { useState, useContext } from 'react';
 import TrybeerContext from '../context/TrybeerContext';
 
 function Login() {
+  const {
+    email,
+    setEmail,
+    setPasword,
+  } = useContext(TrybeerContext);
+  const [enableButton, setEnableButton] = useState(true);
+
+  function handleChange() {
+    let passwordValid;
+    let emailValid;
+    passwordValid = document.getElementById('password-input').value;
+    emailValid = document.getElementById('email-input').value;
+    const regexValidation = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    const minChars = 6;
+    console.log(typeof passwordValid);
+    console.log('Email', regexValidation.test(emailValid));
+    console.log('Senha', passwordValid.length > minChars);
+    if (regexValidation.test(emailValid) && passwordValid.length > minChars) {
+      setEnableButton(false);
+    } else {
+      setEnableButton(true);
+    }
+  }
   return (
     <div>
-      Página de Login
+      <form>
+        <div>
+          <input
+            id="email-input"
+            placeholder="email"
+            type="email"
+            name="email"
+            data-testid="email-input"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input
+            id="password-input"
+            placeholder="password"
+            name="password"
+            type="password"
+            data-testid="password-input"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button
+            disabled={enableButton}
+            type="button"
+            data-testid="signin-btn"
+          //onClick={handleClick}
+          >
+            Entrar
+            </button>
+          <button
+            type="button"
+            data-testid="no-account-btn"
+          //onClick={handleClick}
+          >
+            Ainda não tem conta
+            </button>
+        </div>
+      </form>
+
     </div>
   );
 }
