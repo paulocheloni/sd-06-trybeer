b8c8e3b37a6979fe23724d1ebfa35be8d310e8ec
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-03-12 15:28:09 -0300

    Utilizando Axios com POST no lugar de fetch. Mudança validaçao controlle para service. Criar e salvar token localStorage. .env utilizado no backend

--- back-end/controller/userController.js
+++ back-end/controller/userController.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const userService = require('../service/userService');
 const { validateUserInputs, userExist } = require('../middlewares/userValidation');
 
+const createToken = require('../auth/createToken');
+
 const router = Router();
 const rescue = require('express-rescue');
 
@@ -10,12 +12,26 @@ const rescue = require('express-rescue');
   return res.status(200).json(allUsers);
 })); */
 
-router.get('/', rescue(async (req, res) => {
-  const { email } = req.body;
+router.post('/', validateUserInputs, rescue(async (req, res) => {
+  const { email, password } = req.body;
   // console.log(email);
-  const getUser = await userService.findUserByEmail(email);
+  const getUser = await userService.findUserByEmail(email, password);
+  if (getUser.isError) {
+    return res.status(getUser.status).json({ message: getUser.message });
+  }
 
-  return res.status(200).json(getUser);
+  const userDataForFront = {
+    name: getUser.name,
+    email: getUser.email,
+    role: getUser.role,
+  }
+  // console.log(userDataForFront);
+
+  const userToken = createToken(userDataForFront);
+  console.log(userToken);
+
+  return res.status(200).json([userDataForFront, userToken]);
+  // return res.status(200).json(getUser);
 }));
 
 module.exports = router;
--- front-end/src/services/getUser.js
+++ front-end/src/services/getUser.js
@@ -1,9 +1,29 @@
-const path = 'http://localhost:3001/login';
+// const path = 'http://localhost:3001/login';
 
-async function fetchUser () {
-  await fetch(path).then(
-    r => r.json()
-  );
-};
+import axios from "axios";
+
+// async function fetchUser () {
+//   await fetch(path).then(
+//     (r) => console.log('Services getUser', r.json())
+//   );
+// };
+
+// const fetchUser = async (email, password) => {
+//   return fetch('http://localhost:3001/login/', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//       },
+//       body: JSON.stringify({ email, password })
+//     });
+// }
+
+const fetchUser = async (email, password) => axios
+  .post('http://localhost:3001/login/', {email, password})
+  .then((data) => {
+    // console.log('aqui é dentro do getUser', data.data)
+    return data;
+  })
 
 export default fetchUser;
