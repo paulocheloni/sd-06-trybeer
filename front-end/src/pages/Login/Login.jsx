5c8dcc0f9634506c938cce397ad8101846d5f9c1
LeandroParisi <lepcbelisario@gmail.com>
2021-03-12 20:05:44 -0300

    fix: eslint correcting 2

--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -1,64 +1,48 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import fetchLogin from '../../services/Login'
+import fetchLogin from '../../services/Login';
 
 import './Login.css';
 
-import Input from '../../components/Input/Input'
-import Button from '../../components/Button/Button'
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
 
+const validateEmail = (email) => {
+  const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]{0,2})?$/;
+  return regexEmail.test(email);
+};
+
+const validatePassword = (password) => {
+  const regexPassword = /[a-z0-9]+/;
+  const minLength = 6;
+  return (regexPassword.test(password) && password.length >= minLength);
+};
+
+const userRedirect = async (email, password, history) => {
+  const result = await fetchLogin(email, password);
+  if (result.message) return console.log(result.message);
+  localStorage.setItem('user', JSON.stringify(result));
+  if (result.role === 'administrator') return history.push('/admin/profile');
+  if (result.role === 'client') return history.push('/products');
+  return null;
+};
 
 export default function Login() {
-  const [isDisabled, setIsDisabled] = useState(true)
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [userRole, setUserRole] = useState(null)
-
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const history = useHistory();
 
   useEffect(() => {
-    if(validateEmail(email) && validatePassword(password)) {
-      setIsDisabled(false)
+    if (validateEmail(email) && validatePassword(password)) {
+      setIsDisabled(false);
     }
-  }, [email, password])
+  }, [email, password]);
 
   const setField = (field, value) => {
-    if (field === 'Email') {
-      setEmail(value)
-    } else if (field === 'Senha') {
-      setPassword(value)
-    }
-  }
-
-  const validateEmail = (email) => {
-    const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]{0,2})?$/
-
-    return regexEmail.test(email)
-  }
-
-  const validatePassword = (password) => {
-    const regexPassword = /[a-z0-9]+/
-
-    return (regexPassword.test(password) && password.length >= 6)
-  }
-
-  const userRedirect = async () => {
-    const result = await fetchLogin(email, password)
-    console.log(result);
-    
-    if (result.message) return alert(result.message);
-    
-    localStorage.setItem('user', JSON.stringify(result));
-    
-    if (result.role === 'administrator') {
-      history.push('/admin/profile')
-    } else if (result.role === 'client'){
-      history.push('/products')
-    } else {
-      return null
-    }
-  }
-
+    if (field === 'Email') return setEmail(value);
+    return setPassword(value);
+  };
   return (
     <div className="loginForm">
       <form className="loginInput">
@@ -66,13 +50,29 @@ export default function Login() {
           title="Email"
           type="text"
           testId="email-input"
-          value={email}
-          onChange={setField}         
+          value={ email }
+          onChange={ setField }
           placeholder="Informe o email do Usuário"
         />
-        <Input title="Senha" type="password" testId="password-input" value={password} onChange={setField} userRole={userRole} placeholder="Informe a senha"/>
-        <Button title="ENTRAR" testId="signin-btn" isDisabled={isDisabled} onClick={userRedirect}/>
-        <Button title="Ainda não tenho conta" testId="no-account-btn" onClick={() => history.push('/register')}/>
+        <Input
+          title="Senha"
+          type="password"
+          testId="password-input"
+          value={ password }
+          onChange={ setField }
+          placeholder="Informe a senha"
+        />
+        <Button
+          title="ENTRAR"
+          testId="signin-btn"
+          isDisabled={ isDisabled }
+          onClick={ () => userRedirect(email, password, history) }
+        />
+        <Button
+          title="Ainda não tenho conta"
+          testId="no-account-btn"
+          onClick={ () => history.push('/register') }
+        />
       </form>
     </div>
   );
