67a78f2b29a1dc097b38014f8d9992840c1b4c70
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-03-12 08:53:26 -0300

    solving ESLint for Login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Routes from './Routes';
-import "tailwindcss/tailwind.css";
 
 function App() {
   return (
--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Login from './pages/Login'
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path="/login" component={Login} />
+        <Route path="/login" component={ Login } />
 
       </Switch>
     </Router>
-  )
-};
+  );
+}
 
 export default Routes;
