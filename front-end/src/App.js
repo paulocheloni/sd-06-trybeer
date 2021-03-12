b8c43b409f885b512e622b97065481b43defaf69
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-03-12 08:15:25 -0300

    installing tailwind

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
+import Routes from './Routes';
+import "tailwindcss/tailwind.css";
 
 function App() {
   return (
-    <div className="App">
-      Trybeer
-    </div>
+    <Routes />
   );
 }
 
---
+++ front-end/src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './pages/Login'
+
+
+function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+
+      </Switch>
+    </Router>
+  )
+};
+
+export default Routes;
