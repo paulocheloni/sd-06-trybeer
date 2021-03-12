aa47472a949a4ebd2929e0481725d1c339bcd175
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-03-12 18:14:43 -0300

    configurando

--- back-end/src/model/connection.js
+++ back-end/src/model/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 module.exports = mysql.createPool({
-  host: process.env.HOST,
+  host: process.env.HOSTNAME,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: 'Trybeer',
