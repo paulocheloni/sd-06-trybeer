9b25821120329d15c1a4ddf69ffe41f369e52bc8
LeonardoCavachini <cavachini240@gmail.com>
2021-03-13 11:44:50 -0300

    back-end-side

--- back-end/models/useModel.js
+++ back-end/models/useModel.js
@@ -1,11 +1,29 @@
 const connection = require('./connections');
 
 const userLogin = async (userEmail, userPassword) => {
-  const [[{ id, name, email, password, role }]] = await connection
-    .execute('SELECT * FROM users WHERE email=? and password=?', [userEmail, userPassword]);
-  return (id, name, email, password, role);
+  try {
+    const [[{ id, name, email, password, role }]] = await connection
+      .execute('SELECT * FROM users WHERE email=? and password=?', [userEmail, userPassword]);
+    return (id, name, email, password, role);
+  } catch (e) {
+    return null
+  }
+
+};
+
+const userRegister = async (user) => {
+  const { name, email, password, role } = user;
+  try {
+    await connection
+      .execute('INSERT INTO users (name, email, password, role) VALUES (?,?,?,?)',
+        [name, email, password, role]);
+    return user;
+  } catch (e) {
+    return e.message;
+  }
 };
 
 module.exports = {
   userLogin,
+  userRegister,
 };
