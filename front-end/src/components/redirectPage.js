0646130b8000169be691ae4fda571e82a6eeb72f
Daniel Moura <darthur.moura@gmail.com>
2021-03-12 22:06:12 -0300

    Fixing linter issues in login and redirectPage

--- front-end/src/components/redirectPage.js
+++ front-end/src/components/redirectPage.js
@@ -1,14 +1,21 @@
-import React  from 'react';
-import { useHistory } from "react-router-dom";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function RedirectPage({rota,id,conteudo}) {
-    let history = useHistory();
-    function handleClick() {
-      history.push(rota);
-    }
-    return (
-      <button type='button' data-testid={id}  onClick={handleClick}>{conteudo}</button>
-    );
+function RedirectPage({ rota, id, conteudo }) {
+  const history = useHistory();
+  function handleClick() {
+    history.push(rota);
   }
+  return (
+    <button type="button" data-testid={ id } onClick={ handleClick }>{ conteudo }</button>
+  );
+}
 
-export default RedirectPage
+export default RedirectPage;
+
+RedirectPage.propTypes = {
+  id: PropTypes.string.isRequired,
+  rota: PropTypes.string.isRequired,
+  conteudo: PropTypes.string.isRequired,
+};
--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,62 +1,75 @@
 import React, { useState, useEffect } from 'react';
-import { Redirect, useHistory } from "react-router";
+import { useHistory } from 'react-router';
 // Services
 import { saveState } from '../services/localStorage';
 import api from '../services/api';
 import RedirectPage from '../components/redirectPage';
 
 function Login() {
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [disabled, setDisabled] = useState(true);
 
   const history = useHistory();
-  
-  useEffect(() => {
-    validateEmailAndPassword(email, password)
-  }, [email, password]);
 
-
-  const validateEmailAndPassword = (email, password) => {
-    // const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/gi;
+  const validateEmailAndPassword = (userEmail, pass) => {
     const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.?([a-z]+)?$/i;
-
-    if (regex.test(email) && password.length > 5) {
-      return setDisabled(false)
-    } else {
-      setDisabled(true)
+    const passLength = 5;
+    if (regex.test(userEmail) && pass.length > passLength) {
+      return setDisabled(false);
     }
-  }
+    return setDisabled(true);
+  };
 
+  useEffect(() => {
+    validateEmailAndPassword(email, password);
+  }, [email, password]);
 
   const InsertUserLocalStorage = () => {
     api.listLogin(email, password)
       .then((response) => {
         saveState('user', response.data);
         if (response.data.role === 'administrator') {
-          console.log(`admin`)
           history.push('/admin/orders');
-        } 
+        }
         if (response.data.role === 'client') {
-          console.log(`clientes`)
           history.push('/produtos');
-        } 
+        }
       }).catch((err) => {
         console.log(err.response.data);
       });
-  }
+  };
 
   return (
     <div className="App">
       <h1>Login</h1>
-      <input type='text' data-testid="email-input" placeholder='digite seu Email' onChange={(e) => setEmail(e.target.value)} />
-      <input type='text' data-testid="password-input" placeholder='digite seu Password' onChange={(e) => setPassword(e.target.value)} />
-      <button type='button' data-testid="signin-btn" disabled={disabled}  onClick={InsertUserLocalStorage}> Login </button>
-      <RedirectPage rota={'/register'} id={"no-account-btn"} conteudo={'Ainda não tenho conta'} />
-      
+      <input
+        type="text"
+        data-testid="email-input"
+        placeholder="digite seu Email"
+        onChange={ (e) => setEmail(e.target.value) }
+      />
+      <input
+        type="text"
+        data-testid="password-input"
+        placeholder="digite seu Password"
+        onChange={ (e) => setPassword(e.target.value) }
+      />
+      <button
+        type="button"
+        data-testid="signin-btn"
+        disabled={ disabled }
+        onClick={ InsertUserLocalStorage }
+      >
+        Login
+      </button>
+      <RedirectPage
+        rota="/register"
+        id="no-account-btn"
+        conteudo="Ainda não tenho conta"
+      />
     </div>
   );
-};
+}
 
 export default Login;
