f51ad3914dba5195d9e2bb0e466c4cfd0e02385e
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-03-13 15:36:24 -0300

    feat(backend): requests logs on console

---
+++ back-end/src/middlewares/index.js
@@ -0,0 +1,5 @@
+const log = require('./log.middlewares');
+
+module.exports = {
+  log,
+};
--- back-end/src/routes/main.routes.js
+++ back-end/src/routes/main.routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const { log } = require('../middlewares');
+
 const sessionRouter = require('./session.routes');
 
 const routes = express.Router();
+
+routes.use(log);
 
 routes.use('/login', sessionRouter);
 
