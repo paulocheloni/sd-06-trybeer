0e25b1871436177dcd72ebf3dff7657c9d0a28af
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-03-12 23:14:16 -0300

    eslint fixed - g10

--- back-end/Services/userService.js
+++ back-end/Services/userService.js
@@ -7,7 +7,7 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
   const user = await UserModel.loginUser(email, password);
   if (user) res.status(404).json({ message: 'User not found' });
-  
+
   const token = jwt.sign({ user }, secret);
   user.token = token;
 
@@ -20,14 +20,14 @@ const validateEmail = async (req, res, next) => {
   if (user) res.status(409).json({ message: 'email já existe' });
   console.log('passei pela validação do email');
   next();
-}
+};
 
 const registerNewUser = async (req, res) => {
   console.log('cheguei no register user');
   const { name, email, password, role } = req.body;
   await UserModel.registerNewUser(name, email, password, role);
   return res.status(201).json({ message: 'E-mail already in database' });
-}
+};
 
 module.exports = {
   loginUser,
