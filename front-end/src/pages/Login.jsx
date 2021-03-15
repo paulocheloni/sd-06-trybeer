e089fd1aa8efdd17d901426e93c33c64f5860e41
labelle <beelsdelonge@gmail.com>
2021-03-15 16:02:52 -0300

    Terminada refatoraçao requisitos 1 e 2

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,24 +2,14 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import LoginContext from '../context/LoginContext';
 import FormLogin from '../components/pageLogin/FormLogin';
+import visibilityBtnLogin from '../utils/visibilityBtnLogin';
 
 function Login({ history }) {
   const [user, setUser] = useState({ email: '', password: '' });
   const [valid, setValid] = useState(true);
   useEffect(() => {
-    const isValid = async () => {
-      const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-      const email = emailRegex.test(user.email);
-      const senha = user.password;
-      const minLength = 6;
-      if (senha.length >= minLength && email) {
-        setValid(false);
-      } else {
-        setValid(true);
-      }
-    };
-    isValid();
-  }, [user.password, user.email]);
+    visibilityBtnLogin(user, setValid);
+  }, [user]);
 
   const handleChange = ({ target }) => {
     setUser({ ...user, [target.name]: target.value });
--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,28 +2,21 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import RegisterContext from '../context/RegisterContext';
 import FormRegister from '../components/pageRegister/FormRegister';
+import visibilityBtnRegister from '../utils/visibilityBtnRegister';
 
 function Register({ history }) {
   const [newUser, setUser] = useState({ name: '', email: '', senha: '', tipo: 'client' });
   const [valid, setValid] = useState('true');
   useEffect(() => {
-    const isValid = () => {
-      const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i.test(newUser.email);
-      const validNome = /^[a-záàâãéèêíïóôõöúçñ ]+$/i.test(newUser.name);
-      const six = 6;
-      const doze = 12;
-      const tam = newUser.name.length >= doze;
-      if (validNome && regexEmail && newUser.senha.length >= six && tam) {
-        setValid(false);
-      } else {
-        setValid(true);
-      }
-    };
-    isValid();
-  }, [newUser.senha, newUser.name, newUser.email]);
+    visibilityBtnRegister(newUser, setValid);
+  }, [newUser]);
 
   const handleChange = ({ target }) => {
-    setUser({ ...newUser, [target.name]: target.value });
+    if (target.name === 'tipo') {
+      console.log(target.checked);
+      if (target.checked === true) setUser({ ...newUser, [target.name]: target.value });
+      else setUser({ ...newUser, [target.name]: 'client' });
+    } else { setUser({ ...newUser, [target.name]: target.value }); }
   };
 
   const handleClick = (e) => {
