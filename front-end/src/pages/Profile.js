9a5cd1ad5426b091a544efc2ac4aee5dce98bd46
VinicinBraga <vinicius@bragamatta.com.br>
2021-03-15 19:34:02 -0300

    Requirement 4 in progress

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SidebarMenu from '../components/SideBarMenu';
+import TopMenu from '../components/TopMenu';
 
 function Profile() {
+  const name = 'Vinicius';
+  const [disabled, setDisabled] = useState(true);
+
+  const onChangeName = ({ target: { value } }) => {
+    if (name === value) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  };
+
   return (
     <div>
-      Profile!
+      <TopMenu
+        titleMenu="Meu perfil"
+      />
+      <SidebarMenu />
+      <div className="content-panel">
+        <label htmlFor="name">
+          Name
+          <input
+            data-testid="profile-name-input"
+            type="text"
+            name="name"
+            placeholder="Nome"
+            id="name"
+            onChange={ onChangeName }
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            readOnly="readonly"
+            data-testid="profile-email-input"
+            type="text"
+            name="email"
+            placeholder="Email"
+            id="email"
+          />
+        </label>
+        <button
+          data-testid="profile-save-btn"
+          type="submit"
+          disabled={ disabled }
+        >
+          Salvar
+        </button>
+      </div>
     </div>
   );
 }
