50d6b0d2311d26a1b149547316f6fd3241778281
Isabela Rosa <isabelarosa954@gmail.com>
2021-03-15 18:22:51 -0300

    requisito 3

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import LoginProvider from './context/LoginProvider';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import AdminOrders from './pages/AdminOrders';
 import UserRegister from './pages/UserRegister';
+import Profile from './pages/Profile';
+import Orders from './pages/Orders';
 import './App.css';
 
 function App() {
@@ -12,10 +14,13 @@ function App() {
     <LoginProvider>
       <BrowserRouter>
         <Switch>
+          <Route exact path="/"><Redirect to="/login" /></Route>
           <Route exact path="/login" component={ Login } />
           <Route exact path="/products" component={ Products } />
           <Route exact path="/admin/orders" component={ AdminOrders } />
           <Route exact path="/register" component={ UserRegister } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/orders" component={ Orders } />
         </Switch>
       </BrowserRouter>
     </LoginProvider>
