96e361096e8938fccbdedafd35919e62b9d1668e
LeandroParisi <lepcbelisario@gmail.com>
2021-03-15 19:34:40 -0300

    req 2: solve lint issues

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 import SideBar from '../Sidebar/Sidebar';
 import './Header.css';
 
 export default function Header({ title, user }) {
   return (
     <div className="header">
-      <button type="button" className="menu-btn" data-testid="top-hamburguer" >
-        <FontAwesomeIcon icon={ faBars } size="lg"/>
+      <button type="button" className="menu-btn" data-testid="top-hamburguer">
+        <FontAwesomeIcon icon={ faBars } size="lg" />
       </button>
-      <h1 className="title" data-testid="top-title">{ title } </h1>
-      <SideBar user={user} />
+      <h1 className="title" data-testid="top-title">
+        { title }
+        {' '}
+      </h1>
+      <SideBar user={ user } />
     </div>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired,
+
+};
--- front-end/src/components/Sidebar/Sidebar.jsx
+++ front-end/src/components/Sidebar/Sidebar.jsx
@@ -1,49 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 
 const clientOptions = [
   {
     name: 'Produtos',
-    redirect: '/products'
+    redirect: '/products',
   },
   {
     name: 'Meus Pedidos',
-    redirect: '/orders'
+    redirect: '/orders',
   },
   {
     name: 'Perfil',
-    redirect: '/profile'
+    redirect: '/profile',
   },
   {
     name: 'Sair',
-    redirect: '/login'
-  }
+    redirect: '/login',
+  },
 ];
 
 const adminOptions = [
   {
     name: 'Pedidos',
-    redirect: '/admin/orders'
+    redirect: '/admin/orders',
   },
   {
     name: 'Perfil',
-    redirect: '/admin/profile'
+    redirect: '/admin/profile',
   },
   {
     name: 'Sair',
-    redirect: '/login'
-  }
-]
+    redirect: '/login',
+  },
+];
 
 export default function Sidebar({ user }) {
   const history = useHistory();
-  const options = user === 'client' ? [...clientOptions] : [...adminOptions]
+  const options = user === 'client' ? [...clientOptions] : [...adminOptions];
   return (
     <aside>
       <ul>
         {options.map((option) => (
-          <li key={option.name}>
-            <button type="button" onClick={ () => history.push(`${ option.redirect }`)}>
+          <li key={ option.name }>
+            <button type="button" onClick={ () => history.push(`${option.redirect}`) }>
               {option.name}
             </button>
           </li>
@@ -52,3 +53,8 @@ export default function Sidebar({ user }) {
     </aside>
   );
 }
+
+Sidebar.propTypes = {
+  user: PropTypes.string.isRequired,
+
+};
