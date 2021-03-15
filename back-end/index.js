5f23e178e5329095742bd8052bd10c7939233a7a
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-03-15 14:36:50 -0300

    changing import order for ESLint

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const LoginController = require('./src/controllers/LoginController');
-require('dotenv').config();
 const cors = require('cors');
+require('dotenv').config();
+const LoginController = require('./src/controllers/LoginController');
 
 const app = express();
 const port = process.env.PORT;
