772da185f6b3a350f4073e53a8dd0d83fc5c0930
HCaetano <hrcgeo@gmail.com>
2021-03-15 18:31:37 -0300

    Front - fix: commit 'api' and 'services' folders

---
+++ front-end/src/api/index.js
@@ -0,0 +1,24 @@
+import * as axiosHandler from 'axios';
+
+const buildAxiosHandler = () => {
+  const axios = axiosHandler.create({
+    baseURL: 'http://localhost:3001',
+  });
+
+  return axios;
+};
+
+function login(user) {
+  const axios = buildAxiosHandler();
+
+  return axios.post('/login', user);
+}
+
+function test() {
+  return '';
+}
+
+export {
+  login,
+  test,
+};
