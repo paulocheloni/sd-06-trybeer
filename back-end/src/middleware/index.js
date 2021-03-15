25588d9852fdabd0de1f7928dfc69d1c49b10c7b
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-03-15 15:55:05 -0300

    feat: validate if email already exists

--- back-end/src/middleware/index.js
+++ back-end/src/middleware/index.js
@@ -1,7 +1,9 @@
+const emailAlreadyExists = require('./emailAlreadyExists');
 const auth = require('./auth');
 const error = require('./error');
 
 module.exports = {
+  emailAlreadyExists,
   auth,
   error,
 };
--- back-end/src/routes/RegisterRoute.js
+++ back-end/src/routes/RegisterRoute.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 
 const { RegisterController } = require('../controller');
+const { emailAlreadyExists } = require('../middleware');
 
 const RegisterRoute = Router();
 
 RegisterRoute.post('/',
+  emailAlreadyExists,
   RegisterController.registerClient);
 
 module.exports = RegisterRoute;
