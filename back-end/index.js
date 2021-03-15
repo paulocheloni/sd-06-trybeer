235b22a81f4dcfe7363bcb7d02f6d50b5edd0b6e
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-03-15 18:59:40 -0300

    Back - feat: Add Swagger documentation

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-require('dotenv').config();
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+require('dotenv').config();
 
 const log = require('./middlewares/log');
 const UserController = require('./controller/UserController');
@@ -8,12 +11,23 @@ const LoginController = require('./controller/LoginController');
 const { NOT_FOUND } = require('./schema/statusSchema');
 
 const app = express();
+const { options } = require('./swagger/SwaggerDocument');
+
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
 const PORT = parseInt(process.env.PORT, 10) || 3001;
 
 app.use(express.json());
 app.use(cors());
 app.use(log);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// serve swagger
+// app.get('/api-docs', (req, res) => {
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(swaggerSpec);
+// });
 app.use('/user', UserController);
 app.use('/login', LoginController);
 
