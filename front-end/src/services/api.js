3c4e3f5f565b8729c45d0e1774d7aeaa6bc8545b
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-03-15 19:09:44 -0300

    fix: eslint

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -2,33 +2,30 @@ const axios = require('axios');
 
 const url = 'http://localhost:3001';
 
-const generateToken = async (email, password) => {
-  return await axios.post(`${url}/login`, {
-    email,
-    password,
-  })
+const generateToken = async (email, password) => axios.post(`${url}/login`, {
+  email,
+  password,
+})
   .then((res) => res.data)
   .catch((err) => {
     if (err.response) {
       return err.response.data;
     }
   });
-};
 
-const registerUser = async (name, email, password, role) => {
-  return await axios.post(`${url}/register`, {
+const registerUser = async (name, email, password, role) => axios
+  .post(`${url}/register`, {
     name,
     email,
     password,
     role,
   })
   .then((res) => res.data)
-  .catch((err) => {
-    if (err.response) {
-      return err.response.data;
+  .catch((error) => {
+    if (error.response) {
+      return error.response.data;
     }
   });
-};
 
 module.exports = {
   generateToken,
