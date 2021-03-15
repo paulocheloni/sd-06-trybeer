09c1d9946560876069e0aee74903bddc1e8e8dc1
Pedro Eduardo <pedroedu77@gmail.com>
2021-03-15 18:26:38 -0300

    fix: products model query getAll

---
+++ back-end/src/controllers/products/error/index.js
@@ -0,0 +1,9 @@
+const productsError = {
+  statusCode: 500,
+  customCode: 'ERROR_SERVER',
+  customMessage: 'Error in server. Please, contact support.',
+};
+
+module.exports = {
+  productsError,
+};
--- back-end/src/controllers/products/products.controllers.js
+++ back-end/src/controllers/products/products.controllers.js
@@ -1,12 +1,12 @@
 const { StatusCodes } = require('http-status-codes');
 const { products } = require('../../services');
-// const { loginError } = require('./error');
+const { productsError } = require('./error');
 
-module.exports = async (_req, res, _next) => {
+module.exports = async (_req, res, next) => {
   try {
     const allProducts = await products.getAll();
     return res.status(StatusCodes.OK).json(allProducts);
   } catch (err) {
-    // return next({ ...productsError, err });
+    return next({ ...productsError, err });
   }
 };
