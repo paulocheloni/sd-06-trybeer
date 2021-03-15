586a4fb14b3bbb4e32ed8227d7eb93e008225f1b
alcantara2509 <alcantara2509@gmail.com>
2021-03-15 11:44:34 -0300

    add qty item and renderCards component

--- front-end/src/components/RenderCards.jsx
+++ front-end/src/components/RenderCards.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
 
-const renderCards = (allProducts) => allProducts.map((prod, id) => (
-  <section className="card-content" key={ id }>
-    <p>{prod.price}</p>
-    <img className="products-img" src={ prod.url_image } alt="Foto do Produto" />
-    <h4>{prod.name}</h4>
-    <section className="cards-btn">
-      <button
-        type="button"
-        onClick={ () => {
-          const items = JSON.parse(localStorage.getItem('items')) || [];
-          items.push(prod);
-          localStorage.setItem('items', JSON.stringify(items));
-        } }
-      >
-        +
-      </button>
-      <p>0</p>
-      <button
-        type="button"
-        onClick={ () => {
-          const items = JSON.parse(localStorage.getItem('items')) || [];
-          items.splice(items.indexOf(items.find((e) => e.id === prod.id)), 1);
-          localStorage.setItem('items', JSON.stringify(items));
-        } }
-      >
-        -
-      </button>
-    </section>
-  </section>
-));
-
+const renderCards = (allProducts, asd, setAsd, itemQty) => {
+  const disabledBtnQty = (prod) => itemQty(prod) === 0;
+  return allProducts.map(
+    (prod, id) => (
+      <section className="card-content" key={ id }>
+        <p>{prod.price}</p>
+        <img className="products-img" src={ prod.url_image } alt="Foto do Produto" />
+        <h4>{prod.name}</h4>
+        <section className="cards-btn">
+          <button
+            type="button"
+            onClick={ () => {
+              const items = JSON.parse(localStorage.getItem('items')) || [];
+              items.push(prod);
+              localStorage.setItem('items', JSON.stringify(items));
+              setAsd(asd + 1);
+            } }
+          >
+            +
+          </button>
+          <p>{itemQty(prod)}</p>
+          <button
+            type="button"
+            disabled={ disabledBtnQty(prod) }
+            onClick={ () => {
+              const items = JSON.parse(localStorage.getItem('items')) || [];
+              items.splice(items.indexOf(items.find((e) => e.id === prod.id)), 1);
+              localStorage.setItem('items', JSON.stringify(items));
+              setAsd(asd - 1);
+            } }
+          >
+            -
+          </button>
+        </section>
+      </section>
+    ),
+  );
+};
 export default renderCards;
--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,14 +1,19 @@
-/* eslint-disable max-lines-per-function */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import fetchProducts from '../methods/products';
-// import renderCards from '../components/RenderCards';
+import renderCards from '../components/RenderCards';
 import './Products.css';
 
 function Products() {
   const [allProducts, setAllProducts] = useState([]);
-  const [chartTotal, setChartTotal] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
   const [asd, setAsd] = useState(0);
+
+  const itemQty = (prod) => {
+    const items = JSON.parse(localStorage.getItem('items'));
+    const qty = items.filter((e) => e.id === prod.id);
+    return qty.length;
+  };
 
   useEffect(() => {
     (async () => {
@@ -20,52 +25,25 @@ function Products() {
     const items = JSON.parse(localStorage.getItem('items'));
     if (items) {
       const ad = items.map((a) => a.price);
-      setChartTotal(ad.reduce((e, f) => +e + +f));
+      if (ad !== []) setCartTotal(ad.reduce((e, f) => +e + +f, 0));
     }
   }, [asd]);
 
-  const renderCards = () => allProducts.map((prod, id) => (
-    <section className="card-content" key={ id }>
-      <p>{prod.price}</p>
-      <img className="products-img" src={ prod.url_image } alt="Foto do Produto" />
-      <h4>{prod.name}</h4>
-      <section className="cards-btn">
-        <button
-          type="button"
-          onClick={ () => {
-            const items = JSON.parse(localStorage.getItem('items')) || [];
-            items.push(prod);
-            localStorage.setItem('items', JSON.stringify(items));
-            setAsd(asd + 1);
-          } }
-        >
-          +
-        </button>
-        <p>0</p>
-        <button
-          type="button"
-          onClick={ () => {
-            const items = JSON.parse(localStorage.getItem('items')) || [];
-            items.splice(items.indexOf(items.find((e) => e.id === prod.id)), 1);
-            localStorage.setItem('items', JSON.stringify(items));
-          } }
-        >
-          -
-        </button>
-      </section>
-    </section>
-  ));
-
   return (
     <>
-      <h1>Products</h1>
+      <h1 style={ { marginLeft: '40px' } }>Products</h1>
       <section className="cards-container">
-        {renderCards()}
-        <Link to="/chart">
-          {chartTotal}
+        {renderCards(allProducts, asd, setAsd, itemQty)}
+        <Link to="/cart" className="cart-link">
+          <button type="button" className="cart-btn" disabled={ asd === 0 }>
+            Ver Carrinho
+            {' '}
+            {cartTotal.toFixed(2)}
+          </button>
         </Link>
       </section>
     </>
   );
 }
+
 export default Products;
