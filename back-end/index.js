f648c7f81cb2d54fa3a10eeed62c78ad73ef8166
pa-volpin <paulo.volpin@gmail.com>
2021-03-16 14:54:54 -0300

    Passando PORT do backend como variavel const

--- back-end/index.js
+++ back-end/index.js
@@ -6,7 +6,8 @@ const { handleError } = require('./src/middlewares');
 const routes = require('./src/routes');
 
 const app = express();
-const port = process.env.API_PORT;
+// const port = process.env.API_PORT;
+const port = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
