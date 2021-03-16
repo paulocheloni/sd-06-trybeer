d92c1e82dfacaa175e49e2e45bc43f06f2d22319
phellipe5k <luizinhophe@hotmail.com>
2021-03-16 19:35:53 -0300

    req4/done

--- front-end/src/Pages/Profile/index.jsx
+++ front-end/src/Pages/Profile/index.jsx
@@ -1,55 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-// import Axios from 'axios';
+import Axios from 'axios';
 import Button from '../../Components/Button';
 import Input from '../../Components/Input';
 import Menu from '../../Components/Menu';
 import * as S from './style';
 
+const updateUser = async (name, email, callback) => {
+  try {
+    const request = await Axios.put('http://localhost:3001/profile', { name, email });
+    const data = await request.data;
+    if (data !== 'nome do usuário atualizado') throw new Error();
+    callback('Atualização concluída com sucesso');
+  } catch (error) {
+    alert('Ocorreu um erro', error);
+  }
+};
+
+const fetchUser = async (setEmail, setName) => {
+  const { token } = localStorage;
+  const request = await Axios
+    .get('http://localhost:3001/profile',
+      { headers: { authorization: token } });
+  const data = await request.data;
+  setEmail(data.email);
+  setName(data.name);
+};
+
+// eslint-disable-next-line max-lines-per-function
 const Profile = () => {
   const history = useHistory();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [status, setStatus] = useState('');
+  const [disabled, setDisabled] = useState(false);
   useEffect(() => {
     if (!localStorage.token || localStorage.token === '') history.push('/');
   }, [history]);
-  const updateUser = async () => {
-    // try {
-    // const request = await Axios.put(url api, { name });
-    // const data = await request.data;
-    // if(typeof data === 'string) throw new Error();
-    // } catch (error) {
-    //    alert('Ocorreu um erro', error)
-    // }
-
-    console.log('update user');
-  };
   useEffect(() => {
-    // const fetchUser = async () => {
-    //   const token = JSON.stringify(localStorage.token);
-    //   const request = await Axios.get(url api, { headers: { 'Authorization':  token } });
-    //   const data = await request.data;
-    //   setEmail(data.email);
-    //   setName(data.name);
-    // };
-  }, []);
+    const MaxTime = 2500;
+    setTimeout(() => setStatus(''), MaxTime);
+  }, [status]);
+  useEffect(() => { fetchUser(setEmail, setName); }, []);
+  const handleName = ({ target }) => {
+    setName(target.value);
+    setDisabled(true);
+  };
   return (
     <S.Container>
       <Menu><S.Title data-testid="top-title">Meu perfil</S.Title></Menu>
       <Input
         name="Nome"
         type="text"
-        onChange={ ({ target }) => setName(target.value) }
+        onChange={ handleName }
         dataTestId="profile-name-input"
         value={ name }
       />
       <Input
+        readonly
         name="Email"
         type="email"
         dataTestId="profile-email-input"
         value={ email }
       />
-      <Button dataTestId="profile-save-btn" onClick={ updateUser }>Salvar</Button>
+      <Button
+        dataTestId="profile-save-btn"
+        onClick={ () => updateUser(name, email, setStatus) }
+        disabled={ !disabled }
+      >
+        Salvar
+      </Button>
+      <h2>{ status }</h2>
     </S.Container>
   );
 };
