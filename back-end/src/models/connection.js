17b7af0da464f6cd9df066d6a1b8cf418d8746de
rafMewtwo <ponciano.raf@gmail.com>
2021-03-16 15:16:56 -0300

    req 3 e 4

--- back-end/src/models/connection.js
+++ back-end/src/models/connection.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2/promise');
 require('dotenv/config');
 
+// const connection = mysql.createPool({
+//   user: 'root',
+//   password: '1234',
+//   host: 'localhost',
+//   database: 'Trybeer',
+// });
+
 const connection = mysql.createPool({
-  user: 'root',
-  password: '1234',
-  host: 'localhost',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
   database: 'Trybeer',
 });
 
