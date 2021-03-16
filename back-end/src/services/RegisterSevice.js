83c64d57eab86046d67d6e288c81f0d1863f023f
Tiago Berwanger <berwangertiago@gmail.com>
2021-03-16 19:14:44 -0300

    backend mods

--- back-end/src/services/RegisterSevice.js
+++ back-end/src/services/RegisterSevice.js
@@ -23,7 +23,6 @@ const RegisterService = async (req, res, _next) => {
     const data = { ...payload, token };
     res.status(STATUS_OK).json(data);
   } catch (error) {
-    console.log(error);
     return res.status(STATUS_INTERNAL_SERVER_ERROR).json({ message: 'Internal Server Error' });
   }
 };
