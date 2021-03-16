34f5048936b2540834b267811b8b3fcdd10728a6
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-03-16 14:50:42 -0300

    creating axios instance with baseURL

--- front-end/src/axios/api.js
+++ front-end/src/axios/api.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://${process.env.REACT_APP_BASE_URL}`,
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+
+  const user = JSON.parse(localStorage.getItem('user'));
+  try {
+    if (user) {
+      config.headers.Authorization = user.token;
+    }
+    return config;
+  } catch (err) {
+    console.log('err', err);
+  }
 });
 
 export default api;
