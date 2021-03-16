f368c7ae6ab3b7f88e679653c5ea1e5f11b6fb2b
deko possas <andrepossaster@gmail.com>
2021-03-16 16:24:40 -0300

    importei o cards

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import MenuTop from '../components/MenuTop';
-
+import Cards from '../components/Cards';
 export default function Products() {
   return (
     <div>
       <MenuTop title="TryBeer" />
       <Cards />
-    
-
     </div>
   );
 }
