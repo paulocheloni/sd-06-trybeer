2373f16d141c5620ca1e99defde364ff7af31357
deko possas <andrepossaster@gmail.com>
2021-03-17 14:53:18 -0300

    re criando a branch

--- back-end/index.js
+++ back-end/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
 const userController = require('./controllers/userController');
