3f3eeef0dd46fc155dc3df9486c54245b1eb2527
paulo <paulocheloni@gmail.com>
2021-03-17 10:22:39 -0300

    fixing eslint and error display

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import InputRegister from '../components/InputRegister';
-import {Redirect, useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import RegisterForm from '../components/RegisterForm';
 import register from '../methods/register';
 import { RegisterSchema } from '../validationsSchemas/register';
 
@@ -9,81 +9,47 @@ function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [seller, setSeller] = useState(false);
-  const [formValidated, setFormValidated] = useState(false)
+  const [formValidated, setFormValidated] = useState(false);
   const [messageError, setMessageError] = useState('');
-  const url = useHistory()
-
-  const handleChange = async () => {
-    try {
-      await RegisterSchema.validate({ name, email, password });
-      setFormValidated(true);      
-      setMessageError('')
-    } catch (err) {
-      setMessageError(err.message);
-      console.log(err.message, messageError)
-    }
-  }
+  const [responseError, setResponseError] = useState('');
+  const url = useHistory();
 
   useEffect(() => {
-    handleChange()
-
-  }, [name, email, password])
-
-  
-
-
-  
+    const handleChange = async () => {
+      try {
+        await RegisterSchema.validate({ name, email, password });
+        setFormValidated(true);
+        setMessageError('');
+      } catch (err) {
+        setMessageError(err.message);
+        console.log(err.message, messageError);
+      }
+    };
+    handleChange();
+  }, [name, email, password, messageError]);
 
   const handleClick = async () => {
-    try {      
+    try {
       const response = await register({ name, email, password, seller });
-      if (response.message) throw response
+      if (response.message) throw response;
+      setResponseError('');
       if (seller) {
-        url.push('/admin/orders')
+        url.push('/admin/orders');
       } else {
-        url.push('/products')
+        url.push('/products');
       }
     } catch (err) {
-      console.log(err.message)
-      setMessageError(err.message)
+      console.log(err.message);
+      setResponseError(err.message);
     }
   };
 
-  
-
   return (
-    <>
-      <h1>Pagina de Registro</h1>
-      <InputRegister
-        name="name"
-        setValue={ setName }
-        value={ name }
-        label="Nome"
-      />
-      <InputRegister
-        name="email"
-        setValue={ setEmail }
-        value={ email }
-        label="Email"
-        type="email"
-      />
-      <InputRegister
-        name="password"
-        setValue={ setPassword }
-        value={ password }
-        label="Senha"
-        type="password"
-      />
-      <InputRegister
-        name="seller"
-        setValue={ setSeller }
-        checked={ seller }
-        label="Quero vender"
-        type="checkbox"
-      />
-      <p>{messageError !== '' ? messageError : null}</p>
-      <button type="button" disabled={!formValidated} data-testid="signup-btn" onClick={ async () => handleClick() }>Cadastrar</button>
-    </>
+    <RegisterForm
+      state={ { name, email, password, seller, formValidated, messageError, responseError } }
+      setState={ { setName, setEmail, setPassword, setSeller, setMessageError } }
+      handleClick={ handleClick }
+    />
   );
 }
 
