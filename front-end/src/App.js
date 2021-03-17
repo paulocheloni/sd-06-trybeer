a5fedcc9a0df05cb6c2646fad3f88309f7e3ddc6
deko possas <andrepossaster@gmail.com>
2021-03-17 18:03:59 -0300

    criado o contexto global

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,19 +6,22 @@ import Orders from './pages/Orders';
 import Products from './pages/Products';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
+import Provider from './Context/Provider'
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/admin/orders" component={ AdminOrders } />
-        <Route exact path="/login" component={ Login } />
-        <Route exact path="/register" component={ Register } />
-        <Route exact path="/products" component={ Products } />
-        <Route exact path="/orders" component={ Orders } />
-        <Route exact path="/profile" component={ Profile } />
-        <Redirect from="/" to="/login" />
-      </Switch>
+      <Provider>
+        <Switch>
+          <Route exact path="/admin/orders" component={ AdminOrders } />
+          <Route exact path="/login" component={ Login } />
+          <Route exact path="/register" component={ Register } />
+          <Route exact path="/products" component={ Products } />
+          <Route exact path="/orders" component={ Orders } />
+          <Route exact path="/profile" component={ Profile } />
+          <Redirect from="/" to="/login" />
+        </Switch>
+      </Provider>
     </BrowserRouter>
   );
 }
