11cf76f48bcf68fed8d98ea162a001665a147de3
Jean Machado <jeanvictor.arq@gmail.com>
2021-03-17 10:55:03 -0300

    :lipstick: update shadow button hamburger

--- front-end/src/Components/MenuTop/styles.js
+++ front-end/src/Components/MenuTop/styles.js
@@ -34,7 +34,7 @@ const CompMenuTop = styled.header`
       transition: filter 0.3s;
 
       &:hover {
-        filter: brightness(0.8);
+        filter: brightness(0.955);
       }
 
       > img {
