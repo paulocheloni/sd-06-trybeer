24de5c267c4edfd25c2df9dfcc00d7e05f8ff769
RenatoCostaDev <renatotrybe@gmail.com>
2021-03-17 15:16:37 -0300

    fix:side-bar-menu

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const LoginController = require('./src/controllers/LoginController');
 const RegisterController = require('./src/controllers/RegisterController');
 require('dotenv').config();
-const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT;
