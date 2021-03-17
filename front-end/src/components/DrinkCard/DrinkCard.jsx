79fd54255d086487b7ddf6d73d6cdcfa9d581d50
LeandroParisi <lepcbelisario@gmail.com>
2021-03-17 18:54:05 -0300

    feat: iniciando req 5

--- front-end/src/components/DrinkCard/DrinkCard.jsx
+++ front-end/src/components/DrinkCard/DrinkCard.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-export default function DrinkCard() {
+// Foto;
+
+// Nome do produto;
+
+// Preço unitário;
+
+// Quantidade atual inserida no carrinho;
+
+// Botão de adicionar (+) e de remover (-) uma unidade do produto no carrinho.
+
+// O valor do produto 'R$ 2,20' deverá conter a tag data-testid="0-product-price"
+
+// A imagem do produto deverá conter a tag data-testid="0-product-img"
+
+// O nome do produto 'Skoll..' deverá conter a tag data-testid="0-product-name"
+
+// O botão 'Mais' deverá conter a tag data-testid="0-product-plus"
+
+// O botão 'Menos' deverá conter a tag data-testid="0-product-minus"
+
+// A quantidade de produtos deverá conter a tag data-testid="0-product-qtd"
+
+// O botão 'Ver Carrinho' deverá conter a tag data-testid="checkout-bottom-btn"
+
+// O valor total do carrinho deverá conter a tag data-testid="checkout-bottom-btn-value"
+
+export default function DrinkCard({ cardPayload: { photo, name, price, quantity} }) {
   return (
     <div>
-      <h1>DrinkCard</h1>
     </div>
   );
 }
