64d166bd056d3f100f190bf09ee2a45682f6ff37
Pedro Risso <pmerchiorattor@gmail.com>
2021-03-17 15:56:42 -0300

    fix: npm reinstall, feat: rtl email test, refactor: email validation change

--- front-end/src/context/Provider.jsx
+++ front-end/src/context/Provider.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
+const validator = require('validator');
+
 function Provider({ children }) {
   const [register, setRegister] = useState({});
   const [validForm, setValidForm] = useState(false);
@@ -11,20 +13,21 @@ function Provider({ children }) {
   useEffect(() => {
     const maxLength = 5;
     function validateForm(emailInput, passwordInput) {
-      const emailRegex = /\S+@\S+\.\S+/;
-      if (emailRegex.test(emailInput)
-        && passwordInput.length > maxLength) return setValidForm(true);
+      // const emailRegex = /\S+@\S+\.\S+/;
+      if (validator.isEmail(emailInput) && passwordInput.length > maxLength) return setValidForm(true);
+      // if (emailRegex.test(emailInput) && passwordInput.length > maxLength) return setValidForm(true);
       setValidForm(false);
     }
     validateForm(email, password);
   }, [email, password]);
 
   const validateRegister = (name, emailInput, passwordInput) => {
-    const emailReg = /\S+@\S+\.\S+/;
-    const nameReg = /^[a-zA-Z ]{2,30}$/;
+    // const emailReg = /\S+@\S+\.\S+/;
+    const nameReg = /^[A-Za-z ]{2,30}$/;
     const maxlength = 12;
     const maxlengthPass = 6;
-    if (name.length >= maxlength && nameReg.test(name) && emailReg.test(emailInput)
+    if (name.length >= maxlength && nameReg.test(name) && validator.isEmail(emailInput)
+    // if (name.length >= maxlength && nameReg.test(name) && emailReg(emailInput)
     && passwordInput.length >= maxlengthPass) return setValidRegister(true);
     setValidRegister(false);
   };
