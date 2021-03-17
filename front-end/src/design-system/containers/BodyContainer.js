9c9bbc39f75a25c3885c30789568245ae6e66ec2
pa-volpin <paulo.volpin@gmail.com>
2021-03-17 16:57:24 -0300

    Componentizaçao do FormLogin

--- front-end/src/design-system/containers/BodyContainer.js
+++ front-end/src/design-system/containers/BodyContainer.js
@@ -4,8 +4,9 @@ import proptypes from 'prop-types';
 const BodyContainer = (props) => {
   const { children } = props;
   return (
-    <div className="flex items-center justify-center
-    w-full min-h-screen bg-app-background">
+    <div
+      className="flex items-center justify-center w-full min-h-screen bg-app-background"
+    >
       { children }
     </div>
   );
--- front-end/src/modules/login/components/Form.js
+++ front-end/src/modules/login/components/Form.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import * as API from '../../../utils';
+import Buttons from './Buttons';
+import EmailInput from './EmailInput';
+import PasswordInput from './PasswordInput';
 
 const patternEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 const patternPassword = /^[0-9]{6,}$/;
@@ -33,70 +36,10 @@ function Form() {
   return (
     <form className="flex flex-col mt-10" onSubmit={ handleSubmit }>
       <div className="flex flex-col space-y-4">
-        <div className="flex flex-col space-y-2">
-          <label htmlFor="email-ipt" className="flex flex-col space-y-2">
-            <p>Email*</p>
-            <input
-              id="email-ipt"
-              data-test-id="email-input"
-              name="email"
-              type="text"
-              value={ form.email }
-              onChange={ handleChangeInput }
-              className="border rounded-md p-2 focus:outline-none
-              focus:border-secondary-dark"
-            />
-          </label>
-        </div>
-        <label htmlFor="password-ipt" className="flex flex-col space-y-2">
-          <p>Secret*</p>
-          <p className="hidden">Senha</p>
-          <div className="flex space-x-2 items-center">
-            <input
-              id="password-ipt"
-              data-test-id="password-input"
-              name="password"
-              type={ !seePassword ? 'password' : 'text' }
-              value={ form.password }
-              onChange={ handleChangeInput }
-              className="border rounded-md p-2 focus:outline-none
-              focus:border-secondary-dark"
-            />
-            <button
-              type="button"
-              onClick={ () => setSeePassword((prev) => !prev) }
-            >
-              Icone
-            </button>
-          </div>
-        </label>
-        <div className="w-full mt-10 flex flex-col space-y-2">
-          <p className={`bg-red-500 rounded-md p-2 ${errorMsg ? '' : 'hidden'}`}>{ errorMsg }</p>
-          <button
-            data-testid="signin-btn"
-            className={ `rounded-md w-full p-2 ${(!errorForm.email && !errorForm.password)
-              ? 'bg-secondary' : 'bg-gray-300'} focus:outline-none` }
-            type="submit"
-            disabled={ errorForm.email || errorForm.password }        >
-            <p className="hidden">ENTRAR</p>
-            <p className="hidden">Entrar</p>
-            <p>Sign In</p>
-          </button>
-          <Link
-            data-testid="no-account-btn"
-            className="rounded-md border-secondary border w-full p-2
-              text-center focus:outline-none"
-            to="/register"
-          >
-            <p>Sign Up</p>
-            <p className="hidden">Ainda não tenho conta</p>
-          </Link>
-          <p
-            className="text-xs"
-          >
-            Forgot your passsword?
-          </p>
-        </div>
+        { EmailInput(handleChangeInput, form.email) }
+        { PasswordInput(handleChangeInput, form.password,
+          seePassword, setSeePassword) }
+        { Buttons(errorMsg, errorForm) }
       </div>
     </form>
   );
