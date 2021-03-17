cd91858bcc3056adad8b496076c0bf1aebae48ac
LeandroParisi <lepcbelisario@gmail.com>
2021-03-17 15:44:17 -0300

    feat: starting requirement 4

--- front-end/src/pages/DashboardClient/Profile/Profile.jsx
+++ front-end/src/pages/DashboardClient/Profile/Profile.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from '../../../components/Header/Header';
+import Input from '../../../components/Input/Input';
+import Button from '../../../components/Button/Button';
+
 
 export default function Profile() {
   return (
     <div>
       <Header title="Meu Perfil" user="client" />
-      <form />
+      <form>
+        <Input title="Name" testId="profile-name-input" />
+        <Input title="Email" testId="profile-email-input" isReadOnly/>
+        <Button title="Salvar" testId="profile-save-btn" />
+
+      </form>
     </div>
   );
 }
