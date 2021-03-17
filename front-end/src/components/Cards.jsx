7cb165e4d2f3cb4ae0db68c0674baf3201c4d1e6
deko possas <andrepossaster@gmail.com>
2021-03-17 16:38:23 -0300

    iniciando o quantidade de produtos

--- front-end/src/components/Cards.jsx
+++ front-end/src/components/Cards.jsx
@@ -7,6 +7,14 @@ export default function Cards() {
   const featchApiProduct = async() => {
     const allProducts = await api.fetchProducts();
     setProducts(allProducts);
+  };
+
+  const somarQtd = (index) => {
+    const qtd = document.getElementById(index);
+    const numero = qtd.innerText;
+    console.log(qtd);
+    numero += 1;
+    qtd.innerText = numero;
   }
 
   useEffect(() => {
@@ -19,11 +27,25 @@ export default function Cards() {
         <div key={product.id} >
           <h4 data-testid={`${index}-product-name`}>{product.name}</h4>
           <h5 data-testid={`${index}-product-price`}>{product.price}</h5>
-          <img data-testid={`${index}-product-img`} src={product.url_image}/>
-          {console.log(product.url_image)}
-          <button data-testid={`${index}-product-minus`} type="button"> - </button>
-          <span data-testid={`${index}-product-qtd`} >X coisas</span>
-          <button data-testid={`${index}-product-plus`} type="button"> + </button>
+          {/* <img data-testid={`${index}-product-img`} src={product.url_image}/> */}
+          <button
+            id={`${index}-minus`}
+            data-testid={`${index}-product-minus`}
+            type="button"
+            // onClick={(id) => mudarQtd(id)}
+          >
+            -
+          </button>
+          <span data-testid={`${index}-product-qtd`} id={index} >0</span>
+          {console.log(document.getElementById(index))}
+          <button
+            id={`${index}-plus`}
+            data-testid={`${index}-product-plus`}
+            type="button"
+            onClick={(index) => somarQtd(index)}
+          >
+            +
+          </button>
 
         </div>
       ))}
