0ec3a25807098414bbdf77e783f9c605a51ca6db
pa-volpin <paulo.volpin@gmail.com>
2021-03-17 21:24:32 -0300

    Pagina de Login - teste validado

--- front-end/src/modules/login/components/Form.js
+++ front-end/src/modules/login/components/Form.js
@@ -5,24 +5,11 @@ import Buttons from './Buttons';
 import EmailInput from './EmailInput';
 import PasswordInput from './PasswordInput';
 
-const patternEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-const patternPassword = /^[0-9]{6,}$/;
-
-const patterns = { email: patternEmail, password: patternPassword };
-
 function Form() {
   const [form, setForm] = useState({ email: '', password: '' });
   const [errorForm, setErrorForm] = useState({ email: true, password: true });
   const [errorMsg, setErrorMsg] = useState('');
-  const [seePassword, setSeePassword] = useState(true);
   const history = useHistory();
-
-  const handleChangeInput = ({ target }) => {
-    const { name, value } = target;
-    const validation = patterns[name].test(value);
-    setForm((prev) => ({ ...prev, [name]: value }));
-    setErrorForm((prev) => ({ ...prev, [name]: !validation }));
-  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -36,9 +23,8 @@ function Form() {
   return (
     <form className="flex flex-col mt-10" onSubmit={ handleSubmit }>
       <div className="flex flex-col space-y-4">
-        { EmailInput(handleChangeInput, form.email) }
-        { PasswordInput(handleChangeInput, form.password,
-          seePassword, setSeePassword) }
+        { EmailInput(setErrorForm, setForm, form.email) }
+        { PasswordInput(setErrorForm, setForm, form.password) }
         { Buttons(errorMsg, errorForm) }
       </div>
     </form>
--- front-end/src/modules/login/components/PasswordInput.js
+++ front-end/src/modules/login/components/PasswordInput.js
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function PasswordInput(handleChangeInput, value, seePassword, setSeePassword) {
+function PasswordInput(setError, setInputValue, inputValue) {
+  const [errorLabel, setErrorLabel] = useState(false);
+  const [seePassword, setSeePassword] = useState(true);
+
+  const pattern = /^[0-9]{6,}$/;
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    const validation = pattern.test(value);
+    setInputValue((prev) => ({ ...prev, [name]: value }));
+    setError((prev) => ({ ...prev, [name]: !validation }));
+  };
+
+  const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(
+      () => {
+        const handler = setTimeout(() => {
+          setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+          clearTimeout(handler);
+        };
+      },
+      [value, delay],
+    );
+
+    return debouncedValue;
+  };
+
+  const delay = 500;
+  const debounceValue = useDebounce(inputValue, delay);
+
+  useEffect(() => {
+    if (errorLabel !== undefined && inputValue !== '') {
+      setErrorLabel(!pattern.test(inputValue));
+    }
+  }, [debounceValue]);
+
   return (
-    <label htmlFor="password-ipt" className="flex flex-col space-y-2">
-      <p>Secret*</p>
-      <p className="hidden">Senha</p>
-      <div className="flex space-x-2 items-center">
-        <input
-          id="password-ipt"
-          data-test-id="password-input"
-          name="password"
-          type={ !seePassword ? 'password' : 'text' }
-          value={ value }
-          onChange={ handleChangeInput }
-          className="border rounded-md p-2 focus:outline-none
-          focus:border-secondary-dark"
-        />
-        <button
-          className="w-4 h-4 rounded-full bg-secondary focus:outline-none"
-          type="button"
-          onClick={ () => setSeePassword((prev) => !prev) }
-        >
-          <p className="hidden">Icone</p>
-        </button>
-      </div>
-    </label>
+    <div className="flex flex-col space-y-2">
+
+      <label htmlFor="password-ipt" className="flex flex-col space-y-2">
+        <p>Password*</p>
+        <p className="hidden">Senha</p>
+        <div className="flex space-x-2 items-center">
+          <input
+            id="password-ipt"
+            data-testid="password-input"
+            name="password"
+            type={ !seePassword ? 'password' : 'text' }
+            value={ inputValue }
+            onChange={ handleChange }
+            className="border rounded-md p-2 focus:outline-none
+            focus:border-secondary-dark"
+            placeholder="Enter your password..."
+            required
+            onKeyUp={ () => setErrorLabel(false) }
+          />
+          <button
+            className="w-4 h-4 rounded-full bg-secondary focus:outline-none"
+            type="button"
+            onClick={ () => setSeePassword((prev) => !prev) }
+          >
+            <p className="hidden">Icone</p>
+          </button>
+        </div>
+      </label>
+      <p className={ errorLabel ? 'text-xs text-red-500' : 'hidden' }>
+        At least  6 numbers
+      </p>
+    </div>
   );
 }
 
