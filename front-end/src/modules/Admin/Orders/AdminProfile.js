6ba65ea30cf5858e569a09a446a505d84bf0f883
Tiago Berwanger <berwangertiago@gmail.com>
2021-03-18 16:41:16 -0300

    req10 - admin-profile

--- front-end/src/modules/Admin/Orders/AdminProfile.js
+++ front-end/src/modules/Admin/Orders/AdminProfile.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import SideBarAdmin from '../../../design-components/SideBarAdmin'
+import ContextBeer from '../../../context/ContextBeer';
 
 function AdminProfile() {
+  const history = useHistory();
+  const {
+    getUser,
+  } = useContext(ContextBeer);
+
+  useEffect(() => {
+    if (!getUser()) {
+      history.push('/');
+    } 
+  },[getUser()])
+
   return (
     <div>
       <SideBarAdmin />
+      <h1>Perfil</h1>
+      <p data-testid="profile-name">Nome: {getUser() && getUser().name}</p>
+      <p data-testid="profile-email">Email: {getUser() && getUser().email}</p>
     </div>
   );
 }
