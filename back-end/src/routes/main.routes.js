516c0a5b5ba34432d2c47ae54c52f85b40b082d7
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-03-18 11:37:06 -0300

    feat(backend): admin get all sales

--- back-end/src/routes/main.routes.js
+++ back-end/src/routes/main.routes.js
@@ -6,6 +6,7 @@ const sessionRouter = require('./session.routes');
 const usersRouter = require('./users.routes');
 const productsRouter = require('./products.routes');
 const salesRouter = require('./sales.routes');
+const adminRouter = require('./admin.routes');
 
 const routes = express.Router();
 
@@ -15,6 +16,7 @@ routes.use('/login', sessionRouter);
 routes.use('/products', productsRouter);
 routes.use('/sales', salesRouter);
 routes.use('/user', usersRouter);
+routes.use('/admin', adminRouter);
 
 routes.use(handleError);
 
