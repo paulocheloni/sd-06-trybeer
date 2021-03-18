8df0fcce165d6abce72845f584554e17ea91c8ea
Danilo Couto Pereira Santos <67793508+danilocouto1@users.noreply.github.com>
2021-03-18 19:11:54 -0300

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import { Login, Register, Products, ClientProfile, AdminProfile, Orders, OrdersAdm } from './pages';
+import { Login, Register,
+        Products, ClientProfile, AdminProfile, Orders, OrdersAdm } from './pages';
 
 function App() {
   return (
