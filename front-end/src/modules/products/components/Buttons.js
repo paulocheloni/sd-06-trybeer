cbc6c633fa46b069034942d3f8b96886e697ecf1
Anderson Regis <misterregisslv@gmail.com>
2021-03-18 17:57:05 -0300

    totalPrice adicionado

--- front-end/src/modules/products/components/Buttons.js
+++ front-end/src/modules/products/components/Buttons.js
@@ -1,18 +1,64 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
+import GlobalContext from '../../../context/Context';
 
-const Buttons = () => {
+const Buttons = ({index, prod, allProd}) => {
   const [quantity, setQuantity] = useState(0);
+  const [nameProd, setNameProd] = useState();
+  // const [allProd, setAllProd] = useState();
 
-  const handleClick = (event) => {
-    const { name } = event.target;
-    if (name === '+') setQuantity(quantity + 1);
-    if (name === '-' && quantity > 0) setQuantity(quantity - 1);
+  const { totalPrice, setTotalPrice } = useContext(GlobalContext)
+  // const ref = useRef();
+  // const prevName = ref.current;
+  
+  useEffect(() => {
+    console.log(allProd);
+  }, []);
+
+  const handleClickMinus = () => {
+    if (quantity > 0) {
+      setNameProd(prod.name)
+      const num = totalPrice
+      setTotalPrice(num - Number.parseInt(prod.price));
+      setQuantity(quantity - 1);
+    }
   };
+
+  const handleClickPlus = () => {
+    setNameProd(prod.name)
+    const num = totalPrice
+    setTotalPrice(num + Number.parseInt(prod.price));
+    setQuantity(quantity + 1);
+  };
+
+  // const verifyCart = (nameProd,qtd) => {
+  //   allProd.find((product) => {
+  //     return product.name === nameProd, setAllProd(product.quantity = qtd);
+  //   });
+  // };
+
+  useEffect(() => {
+    // if(allProd) verifyCart(nameProd, quantity);
+  }, [allProd])
+
+  
+
+  useEffect(() => {
+    // if (allProd) setOldCart(verifyCart(nameProd, quantity));
+    // if (quantity > 0) setNewProd([{ ...prod, quantity }]);
+  }, [quantity]);
+
   return (
     <>
-      <button type="button" name="-" onClick={ (e) => handleClick(e) }>-</button>
-      <p>{quantity}</p>
-      <button type="button" name="+" onClick={ (e) => handleClick(e) }>+</button>
+      <button 
+        data-testid={`${ index }-product-minus`}
+        type="button"
+        name="-"
+        onClick={ () => handleClickMinus() }>-</button>
+      <p data-testid={`${ index }-product-qtd`}>{quantity}</p>
+      <button data-testid={`${ index }-product-plus`}
+        type="button"
+        name="+"
+        onClick={ () => handleClickPlus() }>+</button>
     </>
   );
 };
--- front-end/src/modules/products/pages/Products.js
+++ front-end/src/modules/products/pages/Products.js
@@ -1,33 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import Buttons from '../components/Buttons';
+import GlobalContext from '../../../context/Context';
 import api from '../../../axios';
 import PaperContainer from '../../../design-system/containers/PaperContainer';
+
 
 const Products = () => {
   const storage = JSON.parse(localStorage.getItem('user'));
   const existToken = storage ? storage.token : false;
   const [prod, setProd] = useState('');
+  const [newProd, setNewProd] = useState();
   const [rendering, setRendering] = useState(false);
+  const { totalPrice, setTotalPrice } = useContext(GlobalContext)
   useEffect(() => {
     api.get('/products').then((resp) => setProd(resp.data));
   }, []);
 
+  const createArray = () => {
+    const newArrayProd = [...prod];
+    setNewProd(newArrayProd);
+  }
+
   useEffect(() => {
     if (Array.isArray(prod)) {
       setRendering(true);
+      createArray();
     }
   }, [prod]);
 
+  useEffect(() => {
+    console.log('newprod',newProd)
+  }, [newProd])
+
   function getProducts() {
-    return prod.map((product, index) => (
-      <div key={ index }>
-        <p><strong>{`R$ ${product.price}`}</strong></p>
-        <img src={ product.url_image } width="100px" alt={ product.name } />
-        <p>{product.name}</p>
-        <Buttons />
-      </div>
-    ));
+    return (
+      <>
+        { prod.map((product, index) => (
+          <div key={ index }>
+            <p data-testid={`${ index }-product-price`}><strong>{`R$ ${product.price}`}</strong></p>
+            <img data-testid={`${ index }-product-img`}src={ product.url_image } width="100px" alt={ product.name } />
+            <p data-testid={`${ index }-product-name`}>{product.name}</p>
+            <Buttons
+              index={ index }
+              prod={ { price: product.price, img: product.url_image, name: product.name } }
+              allProd={prod}
+            />
+          </div>
+        ))}
+        <button data-testid="checkout-bottom-btn" type="button" name="shop">Ver carrinho</button>
+        <p data-testid="checkout-bottom-btn-value">{totalPrice}</p>
+      </>
+    )
   }
 
   return (
