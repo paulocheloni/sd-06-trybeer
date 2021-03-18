ecee3991b379ab0fa40f88c7fe2d8d84a31e9389
flaviannymaciel <flaviannymaciel@gmail.com>
2021-03-18 17:41:28 -0300

    failing last item

--- front-end/src/pages/AdminProfile.js
+++ front-end/src/pages/AdminProfile.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 // import { Header } from '../components';
 import defaultProfile from '../img/profile.png';
 import SideBarAdmin from '../components/SideBarAdmin';
+import { connect } from 'react-redux';
 
 class AdminProfile extends React.Component {
   constructor() {
@@ -11,23 +12,29 @@ class AdminProfile extends React.Component {
   }
 
   render() {
-    const { history } = this.props;
+    const { history, stateActualUser } = this.props;
+    console.log(stateActualUser);
     return (
       <div className="profile-container">
         {/* <Header history={ history } /> */}
         <SideBarAdmin history={ history } />
         <div className="inputs-div">
           <img src={ defaultProfile } alt="profile" />
-          <h2 data-testid="profile-name">Name</h2>
-          <h2 data-testid="profile-email">Email</h2>
+          <h2 data-testid="profile-name">{ `Nome: ${stateActualUser.name}` }</h2>
+          <h2 data-testid="profile-email">{ `Email: ${stateActualUser.email}` }</h2>
         </div>
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  stateActualUser: state.user.actualUser,
+});
+
 AdminProfile.propTypes = {
   history: PropTypes.shape().isRequired,
+  stateActualUser: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default (AdminProfile);
+export default connect(mapStateToProps, null)(AdminProfile);
