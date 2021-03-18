adc68adb62abfd58f5443e63d7674c8761913568
Paulo Frazão <paulo4187@gmail.com>
2021-03-18 15:31:10 -0300

    feat: altered new branch from 'GR07-req05-back'

--- back-end/src/controller/ProductsController.js
+++ back-end/src/controller/ProductsController.js
@@ -4,6 +4,7 @@ const { ProductsService } = require('../service');
 
 const listProducts = rescue(async (_req, res) => {
   const getProducts = await ProductsService.selectProducts();
+
   return res
     .status(201)
     .json(getProducts);
