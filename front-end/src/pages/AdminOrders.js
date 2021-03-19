b1f32a14fe14ede200313158bf90e020a7b66fbe
Leandro <leandro.feitoza8018@gmail.com>
2021-03-19 19:38:21 -0300

    começando o req 11

--- front-end/src/pages/AdminOrders.js
+++ front-end/src/pages/AdminOrders.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 // import { Header } from '../components';
 import SideBarAdmin from '../components/SideBarAdmin';
+import './AdminOrders.css';
 
 class AdminOrders extends React.Component {
   constructor() {
@@ -11,12 +13,40 @@ class AdminOrders extends React.Component {
   }
 
   render() {
-    const { history } = this.props;
+    const { history, stateCart } = this.props;
+    console.log(stateCart)
     return (
       <div>
         {/* <Header history={ history } /> */}
         <SideBarAdmin history={ history } />
-        <p>Orders</p>
+        { console.log(this.state) }
+        <h1>Olá</h1>
+        { stateCart.map((el, index) => (
+          <Link key={ el.orderNumber } to={ `/orders/:${el.orderNumber}` }>
+            <div key={ el.orderNumber } className="cart-item" product>
+              <h4 data-testid={ `${index}-order-number` }>
+                {' '}
+                { el.orderNumber }
+                {' '}
+              </h4>
+              <p data-testid={ `${index}-order-address` }>
+                {' '}
+                { el.deliveryOrder }
+                {' '}
+              </p>
+              <h4 data-testid={ `${index}-product-price` }>
+                {' '}
+                { el.totalPrice }
+                {' '}
+              </h4>
+              <p data-testid={ `${index}-order-status` }>
+                {' '}
+                { el.statusOrder }
+                {' '}
+              </p>
+            </div>
+          </Link>
+        ))}
       </div>
     );
   }
@@ -26,4 +56,9 @@ AdminOrders.propTypes = {
   history: PropTypes.shape().isRequired,
 };
 
-export default connect(null, null)(AdminOrders);
+const mapStateToProps = (state) => ({
+  stateCart: state.products.cartList,
+});
+
+
+export default connect(mapStateToProps, null)(AdminOrders);
