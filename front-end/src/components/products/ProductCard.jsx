934b307426f831cceb9a035f6de65eb1945194f7
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-03-19 18:59:41 -0300

    feat: try req 5

---
+++ front-end/src/components/products/ProductCard.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import { FiPlus, FiMinus } from 'react-icons/fi';
+
+const ProductCard = ({ product, index, plusItemCart, quantity, minusItemCart, handleItem }) => {
+  return (
+    <div>
+      <img
+        alt={ `Cerveja ${product.name}` }
+        data-testid={ `${index}-product-img` }
+        src={ product.image }
+      />
+      <span
+        data-testid={ `${index}-product-price` }
+      >
+        { `R$ ${product.price.toFixed(2).replace('.', ',')}` }
+      </span>
+      <span
+        data-testid={ `${index}-product-name` }
+      >
+        { product.name }
+      </span>
+      <button
+        type="button"
+        name={ product.name }
+        data-testid={ `${index}-product-plus` }
+        onClick={ () => plusItemCart(product) }
+      >
+        <FiPlus />
+      </button>
+      <span
+        data-testid={ `${index}-product-qtd` }
+      >
+        {/* { (state.quantity >= 0) ? state.quantity : 0 } */}
+        {/* { quantity } */}
+        { handleItem(product) }
+      </span>
+      <button
+        type="button"
+        data-testid={ `${index}-product-minus` }
+        onClick={ () => minusItemCart(product) }
+      >
+        <FiMinus />
+      </button>
+    </div>
+  );
+};
+
+export default ProductCard;
