1709ddbee31f1acb58e2f266cde4cea9107e64c0
Micaeli Teixeira <luime6@gmail.com>
2021-03-19 16:37:44 -0300

    feat(front-and): add codes in Checkout page

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import TopBar from '../components/TopBar';
+import { useHistory } from 'react-router-dom';
 import ParseCurrency from '../utils/parseCurrencyToBRL';
 
 function Checkout() {
   const [checkoutProducts, setCheckoutProducts] = useState([]);
+  const totalPrice = JSON.parse(localStorage.getItem('total'));
+  const history = useHistory();
 
   const purchase = () => {
     const products = JSON.parse(localStorage.getItem('productList'));
@@ -16,6 +19,14 @@ function Checkout() {
     setCheckoutProducts(purchase());
   }, []);
 
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+
+    if (!user) history.push('/login');
+
+  }, []);
+
+
   return !checkoutProducts ? <h1>Loading...</h1> : (
     <div>
       <TopBar name="Finalizar Pedido" />
@@ -25,11 +36,32 @@ function Checkout() {
           <li key={index}>
             <span data-testid={`${index}-product-qtd-input`}>{productQuantity}</span>
             <span data-testid={`${index}-product-name`}>{` - ${name}`}</span>
-            <span data-testid={`${index}-product-total-value`}>{` ${ParseCurrency(price)}`}</span>
+            <span data-testid={`${index}-product-total-value`}>{` R$ ${((price * productQuantity).toFixed(2))}`}</span>
+            <span data-testid={`${index}-product-unit-price`}>{` ${ParseCurrency(price)}`}</span>
+            <button type="button" data-testid={`${index}removal-button`}> X </button>
           </li>
         ))}
       </ul>
+      <h1 data-testid="order-total-value" >{`Total: ${ParseCurrency(totalPrice)} ` }</h1>
 
+      <h1>Endereço</h1>
+      <label htmlFor="streetInput">
+        Rua:
+        <input
+          id="streetInput"
+          type="text"
+          data-testid="checkout-street-input"
+        />
+      </label>
+      <label htmlFor="houseNumberInput">
+        Número da casa:
+        <input
+          id="houseNumberInput"
+          type="text"
+          data-testid="checkout-house-number-input"
+        />
+      </label>
+      <button type="submit" data-testid="checkout-finish-btn"> Finalizar Pedido </button>
     </div>
   );
 }
