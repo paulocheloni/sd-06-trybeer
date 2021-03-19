0fa37f6156a9f2aedf593ef45bf2dd738f1b2e80
Matheus Dias <matheuscara2000@gmail.com>
2021-03-19 20:12:19 -0300

    Connection

--- back-end/src/models/connection.js
+++ back-end/src/models/connection.js
@@ -8,18 +8,18 @@ require('dotenv/config');
 //   database: 'Trybeer',
 // });
 
-const connection = mysql.createPool({
-  user: 'root',
-  password: '',
-  host: 'localhost',
-  database: 'Trybeer',
-});
-
 // const connection = mysql.createPool({
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   host: process.env.HOSTNAME,
+//   user: 'root',
+//   password: '',
+//   host: 'localhost',
 //   database: 'Trybeer',
 // });
+
+const connection = mysql.createPool({
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.HOSTNAME,
+  database: 'Trybeer',
+});
 
 module.exports = connection;
