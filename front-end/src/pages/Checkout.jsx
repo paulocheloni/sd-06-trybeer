170785a4328cf467fbc117270fa1f63248104ed0
Micaeli Teixeira <luime6@gmail.com>
2021-03-19 18:30:33 -0300

    feat(front-and): add codes

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -5,6 +5,7 @@ import ParseCurrency from '../utils/parseCurrencyToBRL';
 
 function Checkout() {
   const [checkoutProducts, setCheckoutProducts] = useState([]);
+  const [isVisible, setIsVisible] = useState(true);
   const totalPrice = JSON.parse(localStorage.getItem('total'));
   const history = useHistory();
 
@@ -15,9 +16,37 @@ function Checkout() {
     ;
   };
 
+  const deleteProduct = (name, productQuantity, price) => {
+    const productDelete = checkoutProducts.filter((product) =>{
+    if(product.name === name) {
+      return false
+    } 
+    return true;
+  })
+    setCheckoutProducts(productDelete);
+    const totalValueProduct = (totalPrice - (productQuantity * price)).toFixed(2);
+    localStorage.setItem('total', JSON.stringify(totalValueProduct));
+
+    localStorage.setItem('productList', JSON.stringify(productDelete));
+  }
+
+  const handleSubmit = async () => {
+    const visibleInterval = 2000;
+    setIsVisible(false);
+    setTimeout(() => {
+      setIsVisible(true);
+      history.push('/orders')
+    }, visibleInterval);
+  };
+
   useEffect(() => {
     setCheckoutProducts(purchase());
   }, []);
+
+ 
+  // useEffect(() => {
+  //   deleteProduct();
+  // },[checkoutProducts]);
 
   useEffect(() => {
     const user = localStorage.getItem('user');
@@ -32,15 +61,18 @@ function Checkout() {
       <TopBar name="Finalizar Pedido" />
       <h1>Produtos</h1>
       <ul>
-        {checkoutProducts.map(({ productQuantity, price, name }, index) => (
+        {(checkoutProducts.length > 0)? 
+        checkoutProducts.map(({ productQuantity, price, name }, index) => (
           <li key={index}>
             <span data-testid={`${index}-product-qtd-input`}>{productQuantity}</span>
             <span data-testid={`${index}-product-name`}>{` - ${name}`}</span>
-            <span data-testid={`${index}-product-total-value`}>{` R$ ${((price * productQuantity).toFixed(2))}`}</span>
+            <span data-testid={`${index}-product-total-value`}>{`  ${ParseCurrency((price * productQuantity).toFixed(2))}`}</span>
             <span data-testid={`${index}-product-unit-price`}>{` ${ParseCurrency(price)}`}</span>
-            <button type="button" data-testid={`${index}removal-button`}> X </button>
+            <button type="button" data-testid={`${index}removal-button`} onClick={()=> deleteProduct(name, productQuantity, price)}> X </button>
           </li>
-        ))}
+        ))
+        : <spam> Não há produtos no carrinho </spam>
+       }
       </ul>
       <h1 data-testid="order-total-value" >{`Total: ${ParseCurrency(totalPrice)} ` }</h1>
 
@@ -61,7 +93,8 @@ function Checkout() {
           data-testid="checkout-house-number-input"
         />
       </label>
-      <button type="submit" data-testid="checkout-finish-btn"> Finalizar Pedido </button>
+      <button type="submit" data-testid="checkout-finish-btn" onClick={ handleSubmit }> Finalizar Pedido </button>
+      <div hidden={ isVisible }>Compra realizada com sucesso!</div>
     </div>
   );
 }
