c7be547a0c58e767884780e2975b503589993e5a
Tiago Berwanger <berwangertiago@gmail.com>
2021-03-19 17:48:43 -0300

    linter

--- back-end/src/models/Sales.js
+++ back-end/src/models/Sales.js
@@ -1,11 +1,11 @@
 const connection = require('./connection');
 
-const createSale = async (user_id, total_price, delivery_address, delivery_number) => {
+const createSale = async (USER_ID, TOTAL_PRICE, DELIVERY_ADDRESS, DELIVERY_NUMBER) => {
   await connection
     .execute(`INSERT INTO sales
     (user_id, total_price, delivery_address, delivery_number, sale_date, status)
     VALUES (?, ?, ?, ?, SYSDATE(), ?)`,
-    [user_id, total_price, delivery_address, delivery_number, "Pendente"]);
+    [USER_ID, TOTAL_PRICE, DELIVERY_ADDRESS, DELIVERY_NUMBER, 'Pendente']);
 };
 
-module.exports = { createSale }
+module.exports = { createSale };
