1e89fa6668184d6df22a3a209f5fc497fb9183be
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-03-19 16:05:12 -0300

    finish requirement without testing

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import SidebarMenu from '../components/SideBarMenu';
+import TrybeerContext from '../context/TrybeerContext';
 import TopMenu from '../components/TopMenu';
+import { verifyToken } from '../utils/verifications';
 import './Orders.css';
 
-function Orders() {
+function Orders({ history }) {
+  const [orders, setOrders] = useState([]);
+  const { getFromLocalStorage } = useContext(TrybeerContext);
+  const recoveredUser = getFromLocalStorage('user');
 
-  // const fetchOrders = async () => {
-  //   await fetchFunctions.get('orders', token).then((ordersArray) => {
-  //     setOrders(ordersArray);
-  //   });
-  // }
-  // useEffect(() => {fetchOrders()}, [])
+  const fetchOrders = async () => {
+    const allOrders = await verifyToken('orders', recoveredUser, history);
+      setOrders(allOrders);
+  }
+
+  useEffect(() => {
+    fetchOrders()
+    setTimeout(() => console.log(orders), 3000)
+  },
+    [setOrders])
   return (
     <div>
       <TopMenu 
@@ -19,16 +28,22 @@ function Orders() {
       <SidebarMenu />
       <div className="content-panel">
         <div className="container">
-          <div
-            className="order-card-container"
-            data-testid="0-order-card-container"
-          >
-            <div className="card-id-date">
-              <div data-testid="0-order-number">order_id</div>
-              <div data-testid="0-order-date">order_date</div>
-            </div>
-            <div className="card-total" data-testid="0-order-total-value">Total</div>
-          </div>
+          {
+            orders.map(({sale_date, total_price}, index) => (
+              <div
+                className="order-card-container"
+                data-testid={`${index}-order-card-container`}
+              >
+                <div className="card-id-date">
+                  <div data-testid={`${index}-order-number`}>{index + 1}</div>
+                  <div data-testid={`${index}-order-date`}>{sale_date}</div>
+                </div>
+                <div className="card-total" data-testid="0-order-total-value">{total_price}</div>
+              </div>
+            ))
+          }
+          
+
         </div>
       </div>
     </div>
