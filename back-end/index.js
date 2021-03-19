ccbfb55a3a17cd6a55cbbbda145e7cf8d62d1209
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-03-19 18:06:23 -0300

    g10 - reqs 6 a 8 - backend

--- back-end/index.js
+++ back-end/index.js
@@ -12,6 +12,7 @@ const rescue = require('express-rescue');
 const UserService = require('./Services/userService');
 const ProductsService = require('./Services/productService');
 const Utils = require('./Utils');
+const SalesService = require('./Services/salesService');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -29,6 +30,16 @@ app.put('/profile/edit',
   rescue(UserService.updateUser));
 
 app.get('/products', ProductsService.findAllProducts);
+
+app.post('/orders', 
+  rescue(Utils.verifyToken), 
+  rescue(SalesService.registerNewOrder));
+
+app.get('/orders/:useremail',
+rescue(SalesService.getOrdersByUser));
+
+app.get('/orderdetails/:saleid',
+rescue(SalesService.getOrderDetails));
 
 app.use((err, _req, res, _next) => {
   const codeStatus = (err.codeStatus) ? err.codeStatus : 500;
