b2bdd4f79ebcc9fd700121a472ae6837761e665d
HCaetano <hrcgeo@gmail.com>
2021-03-19 18:34:06 -0300

    Front - fix: merge contents of Products component

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ControllerHeader from '../components/ControllerHeader';
+// import Card from '../components/Card'
+// import ShowCart from '../components/ShowCart'
+import img from '../img/beer.png';
+import * as axiosHandler from 'axios';
 
 function Products() {
+  const products = [
+    { img, price: 2.50, name:"artesanal" },
+    { img, price: 2.50, name:"artesanal" },
+    { img, price: 2.50, name:"artesanal" },
+    { img, price: 2.50, name:"artesanal" }
+  ]
+
+  const buildAxiosHandler = () => {
+    const axios = axiosHandler.create({
+      baseURL: 'http://localhost:3001',
+    });
+  
+    return axios;
+  };
+
+  const getProducts = async () => {
+    const axios = buildAxiosHandler();
+    const token = localStorage.getItem('token')
+    console.log(token)
+
+    axios.get('/products',{
+      headers:{
+        'authorization': token
+    }}).then(async response => console.log(response));
+  }
+
+  useEffect(() => {
+    getProducts()
+  });
+
   return (
     <div>
-      <ControllerHeader />
-      Produtos
+      <ControllerHeader/>
+      {/* {
+        products
+          ? products.map((product, index) => <Card products={ products } index={ index }/>)
+          : 'There are no products to show'
+      } */}
+      {/* <ShowCart /> */}
     </div>
   );
 }
