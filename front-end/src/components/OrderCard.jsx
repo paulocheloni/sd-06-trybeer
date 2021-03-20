150a13a9055919bf523b064cc1255b029ae48dc4
William Gomes <williamjog@hotmail.com>
2021-03-20 20:58:28 -0300

    Requisito 7: Adicionando lógica para exibicação das datas dos pedidos

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -6,6 +6,13 @@ import productsContext from '../context/productsContext';
 export default function OrderCard() {
   const { orders } = useContext(productsContext);
 
+  const formatDate = (date) => {
+    console.log(date);
+    const day = date.getDate();
+    const month = Number(date.getMonth()) + 1;
+    return `${day}/${month}`;
+  };
+
   return (
     <div className="order-card-container">
       { orders.length && orders.map((order, index) => (
@@ -15,7 +22,11 @@ export default function OrderCard() {
             data-testid={ `${index}-order-card-container` }
           >
             <div data-testid={ `${index}-order-number` }>{order.id}</div>
-            <div data-testid={ `${index}-order-date` }>{order.sale_date}</div>
+            <div
+              data-testid={ `${index}-order-date` }
+            >
+              { formatDate(new Date(order.sale_date)) }
+            </div>
             <div data-testid={ `${index}-order-total-value` }>{order.total_price}</div>
           </div>
         </Link>
