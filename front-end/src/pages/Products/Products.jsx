f4e56ad01020c158c385e9114e229262124535eb
LeandroParisi <lepcbelisario@gmail.com>
2021-03-20 11:48:01 -0300

    fix: adds disable functionality to ver carrinho button

--- front-end/src/pages/Products/Products.jsx
+++ front-end/src/pages/Products/Products.jsx
@@ -3,9 +3,8 @@ import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import { getProducts } from '../../services/Products';
 import DrinkCard from '../../components/DrinkCard/DrinkCard';
-import Button from '../../components/Button/Button';
 import { verifyUser } from '../../store/LocalStorage/actions';
-import { getFullCartPrice } from '../../store/LocalStorage/provider';
+import { getFullCartPrice, getCart } from '../../store/LocalStorage/provider';
 
 // O botão 'Ver Carrinho' deverá conter a tag data-testid="checkout-bottom-btn"
 
@@ -41,11 +40,17 @@ export default function Products() {
           setCartSum={ setCartSum }
         />
       ))}
-      <Button
-        title={ `Ver carrinho ${cartSum}` }
-        testId="checkout-bottom-btn"
+      <button
+        data-testid="checkout-bottom-btn"
         onClick={ handleRedirect }
-      />
+        type="button"
+        disabled={ !getCart() || !getCart().length }
+      >
+        Ver Carrinho
+        <p data-testid="checkout-bottom-btn-value">
+          {cartSum || 'R$ 0,00'}
+        </p>
+      </button>
     </div>
   );
 }
