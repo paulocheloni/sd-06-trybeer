8428d6d1cdcf1bb43572bee681e9816399297c98
William Gomes <williamjog@hotmail.com>
2021-03-19 21:24:41 -0300

    Adicionando um espaço entre as strings da query createOrder

--- back-end/model/ordersModel.js
+++ back-end/model/ordersModel.js
@@ -4,10 +4,11 @@ const connection = require('./connection');
 
 const createOrders = async (userId, objOrder) => {
   try {
-    console.log('cheguei no del', userId, objOrder);
-    await connection.execute('INSERT INTO Trybeer.sales'
-      + '(id, user_id, total_price, delivery_address, delivery_number, sale_date, status)' 
+    console.log('cheguei no del', objOrder, userId);
+    await connection.execute('INSERT INTO Trybeer.sales ' 
+      + '(id, user_id, total_price, delivery_address, delivery_number, sale_date, status) ' 
       + 'VALUES (?,?,?,?,?,?,?)', [
+      1,
       userId,
       objOrder.totalPrice,
       objOrder.address,
@@ -16,7 +17,7 @@ const createOrders = async (userId, objOrder) => {
       objOrder.status,
     ]);
   } catch (error) {
-    console.log('erro', error);
+    console.error(error);
   }
 };
 
