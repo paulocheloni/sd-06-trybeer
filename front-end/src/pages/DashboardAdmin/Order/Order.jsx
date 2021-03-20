27eb5b550562be63997ff812a97a33b35970e167
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-03-20 17:53:19 -0300

    creates getSales in order to fetch information for dynamically render AdminOrderCard - improves AdminOrderCard

--- front-end/src/pages/DashboardAdmin/Order/Order.jsx
+++ front-end/src/pages/DashboardAdmin/Order/Order.jsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {useHistory} from 'react-router-dom';
+import { getSales } from '../../../services/Sales';
 import Header from '../../../components/Header/Header';
 import AdminOrderCard from '../../../components/AdminOrderCard/AdminOrderCard';
 
+// const isAdmin = () => {
+//   const storage = JSON.parse(localStorage.getItem('user'));
+//   if(storage.role !== 'administrator') return 
+// }
+
 
 export default function Orders() {
+  const [sales, setSales] = useState([]);
   const history = useHistory();
 
-  // const handleRedirect = (numeroDoPedido) => {
-  //   history.push(`/orders/:${numeroDoPedido}`);
-  // } USAR PARA REDIRECIONAR PARA O PEDIDO QUANDO CLICAR NELE
+  // QUANDO INICIAR A TELA, TRAZER OS PEDIDOS EXISTENTES
+  useEffect(() => {
+    const fetchSales = async () => {
+      const allSales = await getSales();
+      setSales(allSales);
+    };
+    fetchSales();
+  }, []);
+
+  //USAR PARA REDIRECIONAR PARA O PEDIDO QUANDO CLICAR NELE
+  const handleRedirect = (numeroDoPedido) => {
+    history.push(`/orders/:${numeroDoPedido}`);
+  } 
   return (
     <div>
       <Header title="TryBeer" user="admin" />
-      <AdminOrderCard />
+      {(sales.length && sales.map((sale, index) => (
+      <AdminOrderCard
+        sale={ sale }
+        key={ sale.id }
+        index={ index }
+        onClick={handleRedirect(sale.id)}
+      />
+      ))) ||
+      (
+      <span role="img" aria-label="empty">💸Você não possui nenhum pedido🍺</span>)}
     </div>
   );
 }
