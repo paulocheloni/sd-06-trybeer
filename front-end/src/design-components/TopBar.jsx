41dd7b5ec35951db0b2045d4c140960f816e039e
Tiago Berwanger <berwangertiago@gmail.com>
2021-03-22 19:07:56 -0300

    ajusting tailwind

--- front-end/src/design-components/TopBar.jsx
+++ front-end/src/design-components/TopBar.jsx
@@ -7,17 +7,17 @@ function TopBar(props) {
   const { title } = props;
   const [sidebar, setSidebar] = useState(false);
   return (
-    <header className="w-full bg-indigo-800 flex">
+    <header className="w-full bg-black flex">
       { sidebar && <SideBarMenu /> }
       <button
         type="button"
         data-testid="top-hamburguer"
         onClick={ () => setSidebar(!sidebar) }
-        className="bg-indigo-800 h-12 w-12"
+        className="bg-gray-200 h-12 w-12"
       >
         <FaIcons.FaBars />
       </button>
-      <h1 data-testid="top-title" className="text-center flex-grow text-indigo-400">
+      <h1 data-testid="top-title" className="text-center flex-grow text-white text-2xl">
         {title}
       </h1>
     </header>
