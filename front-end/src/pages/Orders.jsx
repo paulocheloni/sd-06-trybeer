b69343b031faf930f2f146c00361a5e28ee38207
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-03-22 16:43:23 -0300

    feat: create orders page

---
+++ front-end/src/pages/Orders.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuTop from '../components/menu/MenuTop';
+
+const Orders = () => {
+  console.log('');
+  return (
+    <div>
+      <MenuTop name="Meus pedidos" />
+    </div>
+  );
+};
+
+export default Orders;
