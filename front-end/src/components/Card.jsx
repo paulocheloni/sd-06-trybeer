69ee660dea4dddd7bcf595df665d32921595dd5c
HCaetano <hrcgeo@gmail.com>
2021-03-22 17:29:27 -0300

    Front - feat: add markup for Card component and Products page

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -19,31 +19,37 @@ function Card(props) {
 
   return (
     <div className="card-container">
-      <img
-        src={ `${urlImage}` }
-        alt="imagem cerveja"
-        data-testid={ `${id - 1}-product-img` }
-      />
-      <p data-testid={ `${id - 1}-product-price` }>
-        R$
-        { ` ${price.replace('.', ',')}` }
-      </p>
-      <p data-testid={ `${product.id - 1}-product-name` }>{ product.name }</p>
-      <button
-        data-testid={ `${product.id - 1}-product-minus` }
-        type="button"
-        onClick={ () => reduceProduct(params) }
-      >
-        -
-      </button>
-      <span data-testid={ `${id - 1}-product-qtd` }>{quantity}</span>
-      <button
-        data-testid={ `${id - 1}-product-plus` }
-        type="button"
-        onClick={ () => addProduct(params) }
-      >
-        +
-      </button>
+      <div>
+        <img
+          src={ `${urlImage}` }
+          alt="imagem cerveja"
+          data-testid={ `${id - 1}-product-img` }
+        />
+      </div>
+      <section className="product-info">
+        <p data-testid={ `${id - 1}-product-price` }>
+          R$
+          { ` ${price.replace('.', ',')}` }
+        </p>
+        <p data-testid={ `${product.id - 1}-product-name` }>{ product.name }</p>
+        <section>
+          <button
+            data-testid={ `${product.id - 1}-product-minus` }
+            type="button"
+            onClick={ () => reduceProduct(params) }
+          >
+            -
+          </button>
+          <span data-testid={ `${id - 1}-product-qtd` }>{quantity}</span>
+          <button
+            data-testid={ `${id - 1}-product-plus` }
+            type="button"
+            onClick={ () => addProduct(params) }
+          >
+            +
+          </button>
+        </section>
+      </section>
     </div>
   );
 }
