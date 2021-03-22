8720c89b64a29d608a1461b8ae84e95d7fecb2e1
Pedro Risso <pmerchiorattor@gmail.com>
2021-03-22 18:48:09 -0300

    feat: MenuAdmin,  /admin/profile and /admin/orders routes

---
+++ front-end/src/Pages/AdminOrders/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MenuAdmin from '../../Components/MenuAdmin';
+
+const AdminOrders = () => (
+  <div>
+    <MenuAdmin />
+    TODO
+  </div>
+);
+
+export default AdminOrders;
---
+++ front-end/src/Pages/AdminProfile/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MenuAdmin from '../../Components/MenuAdmin';
+
+const AdminProfile = () => (
+  <div>
+    <MenuAdmin />
+    Admin Profile
+  </div>
+);
+
+export default AdminProfile;
