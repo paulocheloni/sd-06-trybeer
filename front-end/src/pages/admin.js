47b42edd528440b363a1b4a25d73c0abd8733d0f
Daniel Moura <darthur.moura@gmail.com>
2021-03-23 18:53:41 -0300

    Material UI added to admin.js

--- front-end/src/pages/admin.js
+++ front-end/src/pages/admin.js
@@ -1,11 +1,48 @@
+// React imports
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
-import NavBarAdmin from '../components/menuNavBarAdmin';
-import api from '../services/api';
-import { loadState } from '../services/localStorage';
 
+// Components
+import NavBarAdmin from '../components/menuNavBarAdmin';
+
+// Services
+import { loadState } from '../services/localStorage';
+import api from '../services/api';
+
+// Material-UI
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
+
+
+// CSS - Material-UI
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  number: {
+    width: 128,
+    height: 128,
+    fontSize: 30,
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+}));
+
+
+// React Component Ddefinition
 function Admin() {
+  const classes = useStyles();
   const [orders, setOrders] = useState([]);
   const history = useHistory();
 
@@ -24,25 +61,66 @@ function Admin() {
   return (
     <div>
       <NavBarAdmin content="Trybeer" />
-      <h1>Pedidos</h1>
-      {orders.map((order, index) => (
-        <Link to={ `/admin/orders/${order.id}` } key={ index }>
-          <div>
-            <h3 data-testid={ `${order.id}-order-number` }>
-              {`Pedido ${order.id}`}
-            </h3>
-            <h4 data-testid={ `${order.id}-order-address` }>
-              {`${order.delivery_address} ${order.delivery_number}`}
-            </h4>
-            <h3 data-testid={ `${order.id}-order-total-value` }>
-              {`R$ ${order.total_price}`.replace('.', ',')}
-            </h3>
-            <h3 data-testid={ `${order.id}-order-status` }>
-              {order.status}
-            </h3>
-          </div>
-        </Link>
-      ))}
+      <div className={classes.root}>
+        <Grid container direction="column" spacing={0}>
+          <h1>Pedidos</h1>
+          {orders.map((order, index) => (
+            <Grid item xs={10}>
+                <Paper className={classes.paper} elevation={3}>
+                  <Grid container spacing={3}>
+                    <Grid item>
+                      <ButtonBase className={classes.number}>
+                        <Link
+                          to={ `/admin/orders/${order.id}` }
+                          style={{ textDecoration: 'none' }}
+                          key={ index }
+                        >
+                          <h1 style={{ color: 'gray' }}>{order.id}</h1>
+                        </Link>
+                      </ButtonBase>
+                    </Grid>
+                    <Grid item xs={12} sm container>
+                      <Grid item xs container direction="column" spacing={3}>
+                        <Grid item xs>
+                          <Typography
+                            gutterBottom
+                            variant="subtitle1"
+                            data-testid={ `${index}-order-number` }
+                          >
+                            {`Pedido ${order.id}`}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            gutterBottom
+                            data-testid={ `${index}-order-address` }
+                          >
+                            {`Endereço: ${order.delivery_address}, ${order.delivery_number}`}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            style={ (order.status === 'Pendente')
+                            ? { color: 'yellow' } : { color: 'green' }}
+                            data-testid={ `${index}-order-status` }
+                          >
+                            {order.status}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Grid item>
+                      <Typography
+                        variant="subtitle1"
+                        data-testid={ `${index}-order-total-value` }
+                      >
+                        {`R$ ${order.total_price}`.replace('.', ',')}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Paper>
+              </Grid>
+          ))}
+        </Grid>
+      </div>
     </div>
   );
 }
