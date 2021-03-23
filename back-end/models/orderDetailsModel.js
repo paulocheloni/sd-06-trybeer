d25e8aaf53d6a05c57a1c05e152c65e374032e11
deko possas <andrepossaster@gmail.com>
2021-03-23 18:34:16 -0300

    criei o service

---
+++ back-end/models/orderDetailsModel.js
@@ -0,0 +1,23 @@
+const connection = require('./connection');
+
+const saleDetails = async () => {
+  const [productsOfSale] = await connection.execute(
+    `SELECT
+    a.sale_date,
+    b.sale_id,
+    b.product_id,
+    b.quantity,
+    c.name,
+    c.price
+    FROM sales_products AS b
+    INNER JOIN sales AS a
+    ON b.sale_id = a.id
+    INNER JOIN products AS c
+    ON b.product_id = c.id `,
+  );
+  return productsOfSale;
+};
+
+module.exports = {
+  saleDetails,
+};
