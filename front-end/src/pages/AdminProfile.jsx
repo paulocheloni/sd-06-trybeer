900d83feb14223a0f5b533718322df4e389e50b9
vapatty6848 <vapatty@gmail.com>
2021-03-23 11:04:03 -0300

    erros de lint

--- front-end/src/pages/AdminProfile.jsx
+++ front-end/src/pages/AdminProfile.jsx
@@ -3,41 +3,36 @@ import { useHistory } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import TopMenuAdmin from '../components/TopMenuAdmin';
 
-
 export default function AdminProfile() {
   const history = useHistory();
   const tokenFromLocalStorage = localStorage.getItem('token');
   const handleName = () => {
-    if(tokenFromLocalStorage) {
+    if (tokenFromLocalStorage) {
       const tokenDecoded = jwtDecode(tokenFromLocalStorage);
-      return tokenDecoded.name; 
+      return tokenDecoded.name;
     }
-  }
+  };
 
   const handleEmail = () => {
-    if(tokenFromLocalStorage) {
+    if (tokenFromLocalStorage) {
       const tokenDecoded = jwtDecode(tokenFromLocalStorage);
-      return tokenDecoded.email; 
+      return tokenDecoded.email;
     }
-  }
+  };
   const handleRedirect = (token) => {
     if (!token) return history.push('/login');
   };
 
   return (
     <div>
-      { handleRedirect(tokenFromLocalStorage) }
-      {<TopMenuAdmin pageTitle="TryBeer" /> }
-        <div>
-          <span data-testid="profile-name">
-            { `Nome: ${handleName()}` }
-          </span>
-        </div>
-        <div>
-          <span data-testid="profile-email">
-            { `Email: ${handleEmail()}` }
-          </span>
-        </div>
+      {handleRedirect(tokenFromLocalStorage)}
+      {<TopMenuAdmin pageTitle="TryBeer" />}
+      <div>
+        <span data-testid="profile-name">{`Nome: ${handleName()}`}</span>
+      </div>
+      <div>
+        <span data-testid="profile-email">{`Email: ${handleEmail()}`}</span>
+      </div>
     </div>
   );
 }
