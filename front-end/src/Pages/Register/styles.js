83d654a52e45c7ed5f78c081b982dce8df791b30
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-03-23 03:15:03 -0300

    checkbox color green on /register g10

--- front-end/src/Pages/Register/styles.js
+++ front-end/src/Pages/Register/styles.js
@@ -50,10 +50,16 @@ const Form = styled.form`
 const Label = styled.label`
   ${({ theme }) => css`
     color: ${theme.colors.text};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
+    display: block;
+    position: relative;
+    padding-left: 35px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    font-size: 22px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
     margin-bottom: 20px;
     font-size: 18px;
     font-weight: 500;
@@ -62,14 +68,21 @@ const Label = styled.label`
       font-size: 16px;
     }
 
-    .input-checkBox {
-      width: 20px;
+    input {
+      position: absolute;
+      opacity: 0;
+      cursor: pointer;
+    }
+
+    .checkmark {
+      position: absolute;
+      top: 0;
+      left: 0;
       height: 20px;
-      font-size: 16px;
-      margin-right: 10px;
-      border: none;
-      border-radius: 5px;
-      box-shadow: 0 0 2px 0;
+      width: 20px;
+      background-color: #eee;
+      border: 1px solid ${theme.colors.borderInput};
+      border-radius: 2px;
 
       @media (max-width: 500px) {
         width: 17px;
@@ -77,6 +90,44 @@ const Label = styled.label`
       }
     }
   `}
+
+  &:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+
+  input:checked ~ .checkmark {
+    background-color: #fff;
+  }
+
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  input:checked ~ .checkmark:after {
+    display: block;
+  }
+
+  .checkmark:after {
+    left: 5px;
+    top: 1px;
+    width: 5px;
+    height: 10px;
+    border: solid #397330;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+
+    @media (max-width: 500px) {
+      left: 4px;
+      top: 1px;
+      width: 4px;
+      height: 8px;
+    }
+  }
+  
 `;
 
 export default {
