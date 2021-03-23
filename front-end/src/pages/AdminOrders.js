3a590f2354a68ddc5a173269cb77c8bd76c59100
Lais Gurgel <laisgurgelf@gmail.com>
2021-03-23 18:43:05 -0300

    Fixing lint

--- front-end/src/pages/AdminOrders.js
+++ front-end/src/pages/AdminOrders.js
@@ -22,8 +22,7 @@ function AdminOrders({ history }) {
     <div>
       <TopMenu />
       {
-        orders
-        .map(({ id, delivery_address: deliveryAddress,
+        orders.map(({ id, delivery_address: deliveryAddress,
           delivery_number: deliveryNumber, total_price: totalPrice, status }, index) => (
           <div key={ id }>
             <Link to={ `/admin/orders/${id}` }>
