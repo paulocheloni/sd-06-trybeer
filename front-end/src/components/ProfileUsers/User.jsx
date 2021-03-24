237c517f293864f27819343709fff6897618bd68
vapatty6848 <vapatty@gmail.com>
2021-03-24 17:47:42 -0300

    correçoes

---
+++ front-end/src/components/ProfileUsers/User.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import jwtDecode from 'jwt-decode';
+import { isTheNewNameDifferent, nameValidation } from '../../utils/validations';
+import useInput from '../../hooks/useInput';
+import fetches from '../../services/fetches';
+import TextField from '@material-ui/core/TextField';
+import useStyles from './styles';
+import Button from '@material-ui/core/Button';
+// import Box from '@material-ui/core/Box'; 
+// import { FormControl } from '@material-ui/core'
+import Container from '@material-ui/core/Container';import { FormControl } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography'; 
+
+export default function User() {
+  const tokenFromLocalStorage = localStorage.getItem('token');
+  const tokenDecoded = jwtDecode(tokenFromLocalStorage);
+  const oldName = tokenDecoded.name;
+  const classes = useStyles();
+  const [name, setName] = useInput(tokenDecoded.name);
+
+  const [newInfo, setNewInfo] = useState('');
+ console.log(isTheNewNameDifferent(name, oldName));
+  const handleUpdateInfo = async (email) => {
+    await fetches.updateUserName(email, name);
+    setNewInfo('Atualização concluída com sucesso');
+  };
+
+  return(
+    // <Box justifyContent="flex-end">
+    <Container className={classes.container}>
+    <FormControl className={classes.root} noValidate autoComplete="off">
+    <div>
+     <TextField
+          id="name"
+          size="medium"
+          variant="filled"
+          label="Nome"
+          defaultValue={ name }
+          data-testid="profile-name-input"
+          onChange={ setName }
+        />
+     </div>
+     <div>
+     <TextField
+          id="email"
+          size="medium"
+          variant="filled"
+          label="Email"
+          defaultValue={ tokenDecoded.email }
+          data-testid="profile-email-input"
+          InputProps={{
+            readOnly: true,
+          }}    
+        />
+     </div>
+    </FormControl>
+     <div>
+     <Button variant="contained" color="primary"
+      data-testid="profile-save-btn"
+      disabled={ !(nameValidation(name) && isTheNewNameDifferent(oldName, name)) }
+      onClick={ () => handleUpdateInfo(tokenDecoded.email) }
+     >
+       Salvar
+       <Typography>{newInfo}</Typography>
+    </Button>
+     </div>
+    </Container>
+    // </Box>
+  );
+}
