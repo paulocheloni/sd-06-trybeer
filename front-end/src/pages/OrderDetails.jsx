fa821e244bab718bfc58188b63804ee457d17ae4
danieleperse <danieleperse3@gmail.com>
2021-03-24 15:23:27 -0300

    requisito 8 - map criado

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -24,15 +24,31 @@ export default function OrderDetails() {
 
   useEffect(() => {
     fetchApiProductOfSale(id);
-  }, []);
+  });
+
+  const sumOfCart = (sum, product) => sum + product.quantity * product.price;
 
   return (
     <div>
       <h1 data-testid="top-title">Detalhes de Pedido</h1>
       <h2 data-testid="order-number">Pedido { id }</h2>
-      <h2
-        data-testid="order-date"
-      >{ productsOfSale.length !== 0 && formatDate(productsOfSale[0].sale_date) }
+      <h2 data-testid="order-date">
+        { productsOfSale.length !== 0 && formatDate(productsOfSale[0].sale_date) }
+      </h2>
+      <div>
+        {productsOfSale.map((produto, index) => (
+          <div key={produto.id}>
+            <div data-testid={`${index}-product-qtd`}>{produto.quantity}</div>
+            <div data-testid={`${index}-product-name`}>{produto.name}</div>
+            <div data-testid={`${index}-product-total-value`}>{ (produto.price * produto.quantity)
+              .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }</div>
+          </div>
+        ))}
+      </div>
+      <h2 data-testid="order-total-value">
+      { productsOfSale
+          .reduce(sumOfCart, 0)
+          .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }
       </h2>
     </div>
   );
