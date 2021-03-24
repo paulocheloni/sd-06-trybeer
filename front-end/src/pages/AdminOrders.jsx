7c198380958b9336008a80fcf3695efc202cd10b
William Gomes <williamjog@hotmail.com>
2021-03-24 15:19:22 -0300

    fix: corrigindo typos nos requisitos 11 e 12

--- front-end/src/pages/AdminOrders.jsx
+++ front-end/src/pages/AdminOrders.jsx
@@ -21,13 +21,13 @@ export default function AdminOrders() {
         {allSales.map((sale, index) => (
           <Link key={ sale.id } to={ `/admin/orders/${sale.id}` }>
             <div data-testid={ `${index}-order-number` }>
-              {`Pedidos ${sale.id}`}
+              {`Pedido ${sale.id}`}
             </div>
             <div data-testid={ `${index}-order-address` }>
               {`${sale.delivery_address}, ${sale.delivery_number}`}
             </div>
             <div data-testid={ `${index}-order-total-value` }>
-              {`${sale.total_price}`}
+              {`R$ ${Number(sale.total_price).toFixed(2).replace('.', ',')}`}
             </div>
             <div data-testid={ `${index}-order-status` }>
               {`${sale.status}`}
