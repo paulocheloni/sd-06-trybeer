59ee2c4f054837ef8657a9e02a72ee0c531f724c
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-03-24 17:52:22 -0300

    fix admin middleware

--- back-end/index.js
+++ back-end/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const RegisterController = require('./controllers/RegistersController');
 const ProductsController = require('./controllers/ProductsController');
 const LoginController = require('./controllers/LoginController');
-// const { validateLogin } = require('./middlewares/validations');
+const { isUserLoggedIn, isUserAdmin } = require('./middlewares/validations');
 const handleError = require('./middlewares/handleError');
 const OrdersController = require('./controllers/OrdersController');
 const AdminController = require('./controllers/AdminController');
@@ -18,7 +18,7 @@ app.use('/register', RegisterController);
 app.use('/products', ProductsController);
 app.use('/login', LoginController);
 app.use('/orders', OrdersController);
-app.use('/admin', AdminController);
+app.use('/admin', isUserLoggedIn, isUserAdmin, AdminController);
 
 app.use('/images', express.static(__dirname.concat('/images')));
 
--- back-end/models/UsersModel.js
+++ back-end/models/UsersModel.js
@@ -4,6 +4,7 @@ const findByEmail = async (email) => {
   const [user] = await connection.execute(
     'SELECT * FROM users WHERE email=?', [email],
   );
+  console.log(user);
   return user;
 };
 
