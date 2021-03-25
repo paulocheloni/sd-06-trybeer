23a960301ecebd9f032c24b1ba9f372c015c1cf6
Pedro Risso <pmerchiorattor@gmail.com>
2021-03-25 10:40:31 -0300

    fix: broken query after eslint compliance change

--- back-end/models/ordersModel.js
+++ back-end/models/ordersModel.js
@@ -11,10 +11,13 @@ const getOrder = async (userId) => {
 };
 
 const getDetailOrder = async (saleId) => {
+  console.log(saleId)
   try {
-    const [product] = await connection
-    .execute('SELECT * FROM sales_products'
-      + 'LEFT JOIN products ON products.id = sales_products.product_id WHERE sale_id=?', [saleId]);
+    const [product] = await connection.execute(
+      'SELECT * FROM sales_products LEFT JOIN products ON products.id = sales_products'
+        + '.product_id WHERE sale_id=?',
+      [saleId],
+      );
     console.log(product);
     return product;
   } catch (e) {
