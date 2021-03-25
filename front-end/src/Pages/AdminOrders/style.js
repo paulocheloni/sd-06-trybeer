7b949cf0c59ab931b8b5f03bf77ae5789a456c5f
Leonardo Cavachini <cavachini240@gmail.com>
2021-03-25 13:29:09 -0300

    lint errors

--- front-end/src/Pages/AdminOrders/style.js
+++ front-end/src/Pages/AdminOrders/style.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+const Container = styled.div`
 
 display: inline-block;
 padding: 0%;
 width: 100%;
 max-width: 100%;
 `;
+
+export default Container;
