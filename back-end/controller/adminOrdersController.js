0cfb347d1c194dcba80a7d342cbf36a0a260c3b0
Leonardo Cavachini <cavachini240@gmail.com>
2021-03-26 16:00:38 -0300

    nao consigo descubrir o erro

--- back-end/controller/adminOrdersController.js
+++ back-end/controller/adminOrdersController.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const getAllOrders = require('../service/adminOrderService');
+const { checkAuthorization } =require('../middleware/checkAuthorization');
 
 const adminOrdersController = Router();
 
 const SUCCESS = 200;
 
-adminOrdersController.get('/', async (_req, res) => {
+adminOrdersController.get('/', checkAuthorization, async (_req, res) => {
   const allOrders = await getAllOrders();
   res.status(SUCCESS).send(allOrders);
 });
--- back-end/service/orderService.js
+++ back-end/service/orderService.js
@@ -1,7 +1,8 @@
-const { getOrder, getDetailOrder } = require('../models/ordersModel');
+const { getOrder, getDetailOrder, getAllOrder } = require('../models/ordersModel');
 
 const getOrders = (userId) => getOrder(userId);
 
 const getDetailOrders = async (saleId) => getDetailOrder(saleId);
+
 
 module.exports = { getOrders, getDetailOrders };
