497a8949c1ade83471e99c71d9626ea7743ace44
Daniel Jesus <dev.danieljs@gmail.com>
2021-03-26 19:45:19 -0300

    test(backend/products): added Authorization token

--- back-end/test/products.test.js
+++ back-end/test/products.test.js
@@ -1,8 +1,29 @@
 const frisby = require('frisby');
+const connection = require('../src/models/connection');
 const { StatusCodes } = require('http-status-codes');
+const { generateToken } = require('../src/security');
 const url = 'http://localhost:3001';
 
+const clientUser = {
+  email: 'user@test.com',
+  password: 'test123',
+  userId: 2,
+  isVendor: true
+};
+
 describe('Testing products endpoint', () => {
+  beforeAll(() => {
+    frisby.globalSetup({
+      request: {
+        headers: {
+          'Authorization': generateToken(clientUser.userId, clientUser.isVendor),
+          'Content-Type': 'application/json',
+        }
+      }
+    });
+    connection.end();
+  });
+
   it('Should be able to get a list of all products', async () => {
     await frisby
         .get(`${url}/products`)
