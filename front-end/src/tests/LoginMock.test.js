68f122fb7572b3c69bf65b3a698e71e29a3ab8eb
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-03-26 20:53:11 -0300

    Requisito 13 - Faltando so um pouco pra ir

--- front-end/src/tests/LoginMock.test.js
+++ front-end/src/tests/LoginMock.test.js
@@ -1,28 +1,39 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, waitForElement } from '@testing-library/react';
 import renderWithRouter from './RenderWithRouter';
 import App from '../App';
 import axios from 'axios';
 import Login from '../pages/Login';
 
-describe('1 - Teste tela de Login.', () => {
-  jest.mock('axios');
+jest.mock('axios');
 
-  const fakeUsers = [
-    { 
-      name: 'testuser', 
-      email: 'user@test.com',
-      role: 'client',
-    }
-  ];
-  const fakeToken = [
-    {
-      token: 'token',
-    }
-  ];
-      
+const fakeUsers = 
+  { 
+    name: 'testuser', 
+    email: 'user@test.com',
+    role: 'client',
+  };
+const fakeToken = 
+  {
+    token: 'token',
+  };
+
+
+describe('1 - Teste tela de Login.', () => {
+  // beforeEach(() => {
+  //   Object.defineProperty(window, "localStorage", {
+  //     value: {
+  //       getItem: jest.fn(() => null),
+  //       setItem: jest.fn(() => null)
+  //     },
+  //     writable: true
+  //   });
+  // });
+        
   it('Será testado que é gerado um Token ao clicar no button Entrar', async () => {
-    const { getByTestId, history } = renderWithRouter(<App />);
+    axios.post.mockReturnValue([fakeUsers, fakeToken]);
+          
+    const { getByTestId, history, getByText } = renderWithRouter(<App />);
     
     const emailInput = getByTestId('email-input');
     const passwordInput = getByTestId('password-input');
@@ -32,37 +43,7 @@ describe('1 - Teste tela de Login.', () => {
     fireEvent.change(passwordInput, { target: { value: 'test123' } });
     fireEvent.click(buttonEntrar);
     
-    await axios.post.mockImplementationOnce((fakeUsers) => Promise.resolve());
-    // expect(axios).toHaveBeenCalled;
-    // await axios.post.mockResolvedValue({data: fakeUsers});
-    
-    
-    
-    expect(axios.post).toHaveBeenCalledTimes(1);
+    expect(window.localStorage.setItem).toHaveBeenCalledTimes(1);
 
-    // await expect(Login('react')).resolves.toEqual(history.push('/products'));
-    // console.log(fakeData);
-    
-
-
-    // expect(window.localStorage.setItem).toHaveBeenCalledTimes(1);
-    // await waitForElement(() => getByDisplayValue("Richard"));
-
-    
-    // expect(window.localStorage.setItem).toHaveBeenCalledWith(
-    //   "name",
-    //   '"Richard"'
-    // );
   });
 });
-
-
-// beforeEach(() => {
-  //   Object.defineProperty(window, "localStorage", {
-  //     value: {
-  //       getItem: jest.fn(() => null),
-  //       setItem: jest.fn(() => null)
-  //     },
-  //     writable: true
-  //   });
-  // });
