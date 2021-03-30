b47eeef25e0f15cbfb728f64e260c503c63416f4
Leonardo Cavachini <cavachini240@gmail.com>
2021-03-30 18:04:58 -0300

    req-12

---
+++ front-end/src/Pages/AdminOrdersDetail/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div` 
+  display: inline-block;
+  width: 100%;
+  max-width: 100%;
+`;
+export const Buttons = styled.div`
+  margin-top: 35px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 500px;
+
+`;
