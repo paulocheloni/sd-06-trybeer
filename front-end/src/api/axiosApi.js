e0874125b18c82a6ec72451792c11ec88e10c12d
Cássio Rubens <cassiorubensadv@gmail.com>
2021-04-01 23:08:46 -0300

    FEAT: renderizaçao dos produtos

--- front-end/src/api/axiosApi.js
+++ front-end/src/api/axiosApi.js
@@ -21,3 +21,18 @@ export async function signUp(name, email, password, role) {
     });
   return registerUser;
 }
+
+export async function getProducts() {
+  const token = localStorage.getItem('token');
+  const products = await axios.get('http://localhost:3001/products', {
+    headers: {
+      authorization: JSON.parse(token),
+    },
+  })
+    .then((resp) => resp.data)
+    .catch((err) => {
+      console.log(err.response);
+      return err.response;
+    });
+  return products;
+}
--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getProducts } from '../api/axiosApi';
 
 export default function Products() {
+  // const [productsAll, setproductsAll] = useState({});
+  // console.log(productsAll, 'productsAll');
+  // console.log(productsAll[0], 'productsAll [0]');
+
+  const execute = async () => {
+    const products = await getProducts();
+    console.log(products);
+    localStorage.setItem('products', JSON.stringify(products));
+    // setproductsAll(products);
+    return products;
+  };
+
+  useEffect(() => {
+    execute();
+  }, []);
+
+  const localStorageProducts = JSON.parse(localStorage.getItem('products'));
+  console.log(localStorageProducts, 'localStorageProducts');
+
+  const idProfile = localStorageProducts[0].id;
+  console.log(idProfile, 'id');
+  const nameProfile = localStorageProducts[0].name;
+  console.log(nameProfile, 'NAME');
+  const priceProfile = localStorageProducts[0].price;
+  console.log(priceProfile, 'price');
+  const urlImageProfile = localStorageProducts[0].url_image;
+  console.log(urlImageProfile, 'url_image');
+
   return (
     <div>
       <p>Your Code Here</p>
+      <ul>
+        {localStorageProducts.map((localStorageProducts, _index) => {
+          return (
+          <p key={
+            `${localStorageProducts.name}_{localStorageProducts.price}_{localStorageProducts.url_image}`}>
+            {localStorageProducts.name} - {localStorageProducts.price} - {localStorageProducts.url_image}
+          </p>
+        )})}
+      </ul>
     </div>
   );
 }
