42ab5c31d6055d0aeb62ca124c275e14ff0b2d5a
Leandro <leandro@fedora.localdomain>
2021-04-12 22:31:59 -0300

    requisito 9 passando local

--- front-end/src/pages/AdminOrders.js
+++ front-end/src/pages/AdminOrders.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
+import NavbarAdmin from '../components/NavBarAdmin';
 
 export default function AdminOrders() {
   return (
     <div>
-      <p>Your Code Here</p>
+      <Header />
+      <NavbarAdmin />
     </div>
   );
 }
