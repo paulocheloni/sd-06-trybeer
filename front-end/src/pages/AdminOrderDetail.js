41b01c330572b4b796ed5c2a535b7fa649f41203
Leandro <leandro.feitoza8018@gmail.com>
2021-04-17 20:47:50 -0300

    finalizado requisito 12

--- front-end/src/pages/AdminOrderDetail.js
+++ front-end/src/pages/AdminOrderDetail.js
@@ -5,6 +5,7 @@ import NavbarAdmin from '../components/NavBarAdmin';
 import Header from '../components/Header';
 import CardOrder from '../components/CardOrder';
 import { Container } from '../styles/styles';
+
 import './AdminOrderDetail.css';
 import useDataFetch from '../Hooks/useDataFetch';
 
@@ -30,9 +31,8 @@ function AdminOrderDetail() {
         <div>
           {
             result && (
-              <div className="content">
+              <div className="card">
                 <h2
-                  className="order"
                   data-testid="order-number"
                 >
                   {`Pedido ${orderNumber[0]}`}
@@ -42,7 +42,7 @@ function AdminOrderDetail() {
                 >
                   { status || orderStatus[0] }
                 </h2>
-                <ul className="middle-card">
+                <ul>
                   { result.map((product, index) => (
                     <CardOrder
                       key={ index }
@@ -52,10 +52,10 @@ function AdminOrderDetail() {
                   ))}
                 </ul>
                 <div
-                  className="bottom-card total-value item-grid"
+                  className="bottom-card"
                   data-testid="order-total-value"
                 >
-                  {`Valor total do pedido:R$ ${totalValue[0]}`}
+                  {`Valor total do pedido: R$ ${totalValue[0]}`}
                 </div>
                 {
                   status !== 'Entregue' && (
--- front-end/src/pages/AdminOrders.js
+++ front-end/src/pages/AdminOrders.js
@@ -6,6 +6,7 @@ import withAuth from '../components/hocs/withAuth';
 import { Container } from '../styles/styles';
 import Api from '../api/axiosApi';
 import withOrders from '../components/hocs/withHandleApi';
+import './AdminOrderDetail.css';
 
 function AdminOrders(orders) {
   return (
@@ -15,32 +16,34 @@ function AdminOrders(orders) {
       <Container>
         {
           !orders.loading && orders.data.map((order, index) => (
-            <ul key="index">
-              <Link to={ `/admin/orders/${index + 1}` }>
-                <button
-                  type="button"
-                  data-testid={ `${index}-order-number` }
-                >
-                  {`Pedido ${index + 1}`}
-                </button>
-                <p
-                  data-testid={ `${index}-order-address` }
-                >
-                  {`${order.delivery_address}, ${order.delivery_number}`}
-                </p>
-                <p
-                  data-testid={ `${index}-order-total-value` }
-                >
-                  {`R$ ${order.total_price.replace('.', ',')}`}
-                </p>
-                <p
-                  data-testid={ `${index}-order-status` }
-                >
-                  { order.status }
-                </p>
-              </Link>
+            <div key="index" className="card">
+              <ul>
+                <Link to={ `/admin/orders/${index + 1}` }>
+                  <button
+                    type="button"
+                    data-testid={ `${index}-order-number` }
+                  >
+                    {`Pedido ${index + 1}`}
+                  </button>
+                  <p
+                    data-testid={ `${index}-order-address` }
+                  >
+                    {`${order.delivery_address}, ${order.delivery_number}`}
+                  </p>
+                  <p
+                    data-testid={ `${index}-order-total-value` }
+                  >
+                    {`R$ ${order.total_price.replace('.', ',')}`}
+                  </p>
+                  <p
+                    data-testid={ `${index}-order-status` }
+                  >
+                    { order.status }
+                  </p>
+                </Link>
+              </ul>
 
-            </ul>
+            </div>
           ))
         }
       </Container>
